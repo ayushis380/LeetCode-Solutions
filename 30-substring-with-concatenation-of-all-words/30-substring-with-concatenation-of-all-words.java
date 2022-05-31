a450da0d51ad90da5d3789243e8a6c5a30ced213
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2022-05-30 22:08:17 -0600

    Time: 9 ms (97.15%), Space: 42.7 MB (97.03%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.java
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.java
@@ -1,35 +1,52 @@
 class Solution {
     private HashMap<String, Integer> wC = new HashMap<>();
-    private int eachWordLen, subLen, wordsLen;
+    private int sLen, wordsLen, eachWordLen, subStrSize;
     public List<Integer> findSubstring(String s, String[] words) {
-        int sLen = s.length();
+        sLen = s.length();
         wordsLen = words.length;
         eachWordLen = words[0].length();
-        subLen = wordsLen * eachWordLen;
-        List<Integer> res = new ArrayList<>();
+        subStrSize = wordsLen * eachWordLen;
         for(String str: words){
             wC.put(str, wC.getOrDefault(str,0)+1);
         }
-        for(int i=0; i< sLen- subLen+1; i++){
-            if(check(i,s))
-                res.add(i);
+        List<Integer> res = new ArrayList<>();
+        for(int i=0; i< eachWordLen; i++){
+            check(i,s, res);
         }
         return res;
-        
     }
-    public boolean check(int i, String s){
-        HashMap<String, Integer> rem = new HashMap<>(wC);
-        int wordsUsed =0;
-        for(int j=i; j< i+ subLen; j+=eachWordLen){
-            String sub = s.substring(j, j+eachWordLen);
-            if(rem.getOrDefault(sub,0) != 0){
-                rem.put(sub, rem.get(sub)-1);
-                wordsUsed++;
+    private void check(int left, String s, List<Integer> res){
+        HashMap<String, Integer> wF = new HashMap<String, Integer>();
+        int wordsUsed =0; 
+        boolean excessWord= false;
+        for(int right= left; right<= sLen- eachWordLen; right+= eachWordLen){
+            String sub = s.substring(right, right+eachWordLen);
+            if(!wC.containsKey(sub)){
+                wF.clear();
+                wordsUsed =0;
+                excessWord = false;
+                left= right + eachWordLen;
+            } else{
+                while(right-left == subStrSize || excessWord){
+                    String leftmostWord = s.substring(left, left+eachWordLen);
+                    // System.out.println(leftmostWord);
+                    left+= eachWordLen;
+                    wF.put(leftmostWord, wF.get(leftmostWord)-1);
+                    if(wF.get(leftmostWord) >= wC.get(leftmostWord)){
+                        excessWord = false;
+                    }else{
+                        wordsUsed--;
+                    }
+                }
+                wF.put(sub, wF.getOrDefault(sub,0)+1);
+                if(wF.get(sub) > wC.get(sub)){
+                    excessWord = true;
+                }
+                else 
+                    wordsUsed++;
+                if(wordsUsed == wordsLen && !excessWord)
+                    res.add(left);
             }
-            else
-                break;
         }
-        return wordsUsed == wordsLen;
     }
-    
 }
