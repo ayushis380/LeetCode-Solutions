7e18e64fe387463e531d1dba788a084b6c24dd73
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2022-06-03 00:44:53 -0600

    Time: 264 ms (17.95%), Space: 126 MB (64.13%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
@@ -1,15 +1,18 @@
 class NumMatrix {
     public int[][] res;
     public NumMatrix(int[][] matrix) {
-      res   = matrix; 
+        res = new int[matrix.length][matrix[0].length+1];
+        for(int i=0; i< matrix.length; i++){
+            for(int j=0; j< matrix[0].length; j++){
+                res[i][j+1] = res[i][j] + matrix[i][j];
+            }
+        }
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
         int sum=0;
         for(int i=row1; i<= row2; i++){
-            for(int j= col1; j<=col2; j++){
-                sum+= res[i][j];
-            }
+            sum+= res[i][col2 +1] - res[i][col1];
         }
         return sum;
     }
