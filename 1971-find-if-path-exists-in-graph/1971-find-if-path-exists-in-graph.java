36c0a4c1c8020fd6a71ca474b1d5d20edf424a62
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2022-08-14 20:30:27 -0600

    Time: 171 ms (57.83%), Space: 196.7 MB (52.35%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,34 +1,37 @@
 class Solution {
+    boolean seen;
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         boolean[] check = new boolean[n];
-        HashSet<Integer>[] graph = new HashSet[n];
+        HashSet<Integer>[] graph= new HashSet[n];
         
-        for(int i=0; i<n; i++){
+        for(int i=0; i< n ; i++){
             graph[i] = new HashSet<Integer>();
         }
-        for(int[] edge : edges){
-            graph[edge[0]].add(edge[1]);
-            graph[edge[1]].add(edge[0]);
+        for(int[] e: edges){
+            graph[e[0]].add(e[1]);
+            graph[e[1]].add(e[0]);
         }
-        if(graph[source].contains(destination)){
+        if(graph[source].contains(destination))
             return true;
-        }
-        check[source] = true; 
-        int curr;
-        Queue<Integer> q = new LinkedList<Integer>();
-        q.offer(source);
-        while(!q.isEmpty()){
-            curr = q.poll();
-            if(curr == destination)
-                return true;
-            for(Integer neigh: graph[curr]){
-                if(!check[neigh]){
-                    q.offer(neigh);
-                    check[neigh] = true;
-                }
+        
+        seen = false;
+        dfs(graph, check, source, destination);
+        return seen;
+        
+        
+    }
+    public void dfs(HashSet<Integer>[] graph, boolean[] check, int source, int destination){
+        if(!check[source] && !seen){
+            // System.out.println("Source: " + source);
+            if(source == destination){
+                seen = true;
+                return;
             }
         }
-        return false;
-        
+        check[source] = true;
+        for(Integer neigh: graph[source]){
+            if(!check[neigh])
+                dfs(graph, check, neigh, destination);
+        }
     }
 }
