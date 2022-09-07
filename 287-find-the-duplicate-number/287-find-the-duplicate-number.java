36ecb9cd3342e70dfdfb78bd0cc391ac4fe5c1c3
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2022-09-06 23:22:19 -0600

    Time: 6 ms (74.28%), Space: 75.8 MB (48.43%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -4,13 +4,12 @@ class Solution {
         do{
             fast = nums[nums[fast]];
             slow = nums[slow];
-        }while(fast != slow);
-        
+        }while(fast!= slow);
         slow = nums[0];
         while(fast != slow){
             fast = nums[fast];
             slow = nums[slow];
         }
-        return slow;
+        return fast;
     }
 }
