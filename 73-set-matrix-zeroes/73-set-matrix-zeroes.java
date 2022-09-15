fdc5f90e80c572d76859045093e2708fdf8a83cf
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2022-09-15 01:19:06 -0600

    Time: 2 ms (49.44%), Space: 54.4 MB (29.11%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,24 +1,33 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
+        boolean colCheck = false;
         int m = matrix.length;
         int n = matrix[0].length;
-        Set<Integer> rs = new HashSet<Integer>();
-        Set<Integer> cs = new HashSet<Integer>();
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
+        for(int i=0; i< m; i++){
+            if(matrix[i][0] ==0) // to check the first column if any value is 0
+                colCheck = true;
+            for(int j=1; j<n; j++){
                 if(matrix[i][j] ==0){
-                    rs.add(i);
-                    cs.add(j);
+                    matrix[0][j] =0;
+                    matrix[i][0] =0;
                 }
+                    
             }
         }
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(rs.contains(i) || cs.contains(j)){
-                    matrix[i][j] =0;
+            for(int i=1; i<m; i++){
+                for(int j=1; j<n;j++){
+                    if(matrix[i][0]==0 || matrix[0][j] ==0)
+                        matrix[i][j]=0;
                 }
             }
-        }
-        
+            if(matrix[0][0] ==0){ //if first row needs to be marked as 0
+                for(int i=0; i<n; i++)
+                    matrix[0][i] = 0;
+            }
+            if(colCheck){
+            for(int i=0; i<m; i++)
+                    matrix[i][0] = 0;
+            }
+                
     }
 }
