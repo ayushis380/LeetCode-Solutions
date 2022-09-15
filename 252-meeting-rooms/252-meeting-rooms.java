b33483f70f1159273eb78210d1936cf29909012f
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2022-09-15 01:41:33 -0600

    Time: 18 ms (10.19%), Space: 45.4 MB (21.06%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.java
+++ 252-meeting-rooms/252-meeting-rooms.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
         Arrays.sort(intervals,(a,b)->a[0]-b[0]);
-        int j=1;
-        for(int i=1; i< intervals.length; i++){
-            if(intervals[i][j-1] < intervals[i-1][j]) // compare 0,1 with 1,0           
+        // int j=1;
+        for(int i=0; i< intervals.length-1; i++){
+            if(intervals[i+1][0] < intervals[i][1]) // compare 0,1 with 1,0           
                 return false;
             }
         return true;
