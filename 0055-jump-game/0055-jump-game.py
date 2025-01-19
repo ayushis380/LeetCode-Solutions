da8530b7254d2152a105c8f94a2be0051b3060b5
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-01-19 12:58:53 -0500

    Time: 15 ms (75.93%), Space: 17.5 MB (97.43%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,10 +1,10 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        length = len(nums)
-        target = length - 1 # if nums = 0 at last index thats ok as its the last index and we need to reach the last index
+        # move target closer
+        target = len(nums) - 1
 
-        for i in range(length - 1, -1, -1):
-            if i + nums[i] >= target: # index + step value should be able to reach the target
-                target = i
+        for i in range(len(nums) -1, -1, -1):
+            if i + nums[i] >= target: # at that index and its step value
+                target = i # move target closer to start index
         
         return True if target == 0 else False
