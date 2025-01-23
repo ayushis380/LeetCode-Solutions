c1f9ec3ddc0494c05be8057f59a59c99e908bfe8
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-01-22 22:17:18 -0500

    Time: 0 ms (100%), Space: 17.5 MB (60.83%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,13 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # memoization = top down 
-        cache = [-1] * n
-        def dfs(i):
-            if i >= n:
-                return i == n
-            if cache[i] != -1:
-                return cache[i]
-            cache[i] = dfs(i + 1) + dfs(i + 2)
-            return cache[i]
-            
-        return dfs(0)
+        one, two = 0, 1
+
+        for i in range(n):
+            temp = two
+            two = one + two
+            one = temp
+        
+        return two
