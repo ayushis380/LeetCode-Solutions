34ae0b912e7e389dffb2c0e3c5e159ef1fc09b6a
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-01-22 21:43:47 -0500

    Time: 0 ms (100%), Space: 17.8 MB (25.95%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,31 +1,26 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-    # need to focus on indexes, higher chances to get it wrong
-    # keeping right and bottom at +1 than required to fit the range func
-    # dont forget to update the indexes when accessing the value
         left, right = 0, len(matrix[0])
         top, bottom = 0, len(matrix)
         output = []
 
         while left < right and top < bottom:
             for i in range(left, right):
-                output.append(matrix[top][i]) # same row, col changing
+                output.append(matrix[top][i])
             top += 1
 
             for i in range(top, bottom):
-                output.append(matrix[i][right - 1]) # same col, row changing
+                output.append(matrix[i][right - 1])
             right -= 1
-        
-        # eg 1 2 3 or 1 \n 2 \n 3
-        # top and right values are updated so need to check again if they are in boundaries
-            if not( left < right and top < bottom ): 
+
+            if top >= bottom or left >= right:
                 break
             
-            for i in range(right - 1, left - 1, -1):
+            for i in range(right- 1, left-1, -1):
                 output.append(matrix[bottom - 1][i])
             bottom -= 1
 
-            for i in range(bottom - 1, top - 1, -1):
+            for i in range(bottom -1, top - 1, -1):
                 output.append(matrix[i][left])
             left += 1
         
