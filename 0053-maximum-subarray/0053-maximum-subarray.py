ab8e1a2b5a44f321329aabf52c5cc481d065d196
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-01-22 19:16:27 -0500

    Time: 28 ms (89.24%), Space: 32.8 MB (15.99%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,27 +1,14 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        maxsum = float("-inf")
         presum = 0
-        start = end = 0
-        temp_start = 0  # Temporary start index for a new subarray
+        maxsum = float("-inf")
 
-        for i in range(len(nums)):
-            presum += nums[i]
-
+        for val in nums:
+            presum += val
             if presum > maxsum:
-                maxsum = presum
-                start = temp_start  # Update the start index of the max subarray
-                end = i  # Update the end index of the max subarray
+                maxsum = max(maxsum, presum)
             
-            # If presum is less than 0, reset it and update temp_start
             if presum < 0:
                 presum = 0
-                temp_start = i + 1  # Potential start of a new subarray
-
-        # Printing the max subarray and the max sum
-        max_subarray = nums[start:end + 1]
-        print("Max Sum:", maxsum)
-        print("Subarray with Max Sum:", max_subarray)
         
         return maxsum
-
