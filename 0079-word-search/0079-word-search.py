29d0fda9b893d3da77c9d27d3ca0b49941a6b31b
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-01-22 22:33:58 -0500

    Time: 3120 ms (87.03%), Space: 18 MB (24.93%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,29 +1,29 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         rows, cols = len(board), len(board[0])
-        visited = set()
 
-        def dfs(r, c, ind):
-            if ind >= len(word):
+        def dfs(r, c, i):
+            if i >= len(word):
                 return True
             
-            if r >= rows or r < 0 or c >= cols or c < 0 or board[r][c] != word[ind] or (r,c) in visited:
+            if r >= rows or r < 0 or c >= cols or c < 0 or board[r][c] != word[i]:
                 return False
             
-            visited.add((r,c))
+            visited = board[r][c]
+            board[r][c] = "#"
 
-            check = ( dfs(r+1, c, ind+1) or \
-            dfs(r-1, c, ind+1) or \
-            dfs(r, c-1, ind+1) or \
-            dfs(r, c+1, ind+1) )
+            res = ( dfs(r+1, c, i+1) or \
+            dfs(r-1, c, i+1) or \
+            dfs(r, c+1, i+1) or \
+            dfs(r, c-1, i+1) )
             
-            visited.remove((r,c))
-            return check
+            board[r][c] = visited
+            return res
         
-        for i in range(rows):
-            for j in range(cols):
-                if board[i][j] == word[0]:
-                    if dfs(i, j, 0):
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == word[0]:
+                    if dfs(r, c, 0):
                         return True
         
         return False
