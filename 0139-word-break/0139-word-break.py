8439fc73dade60591d9ef67a8c3f05c395d3cefb
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-01-24 17:19:41 -0500

    Time: 3 ms (69.68%), Space: 17.8 MB (44.31%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -2,15 +2,13 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         length = len(s)
         dp = [False] * (length + 1)
-        dp[length] = True # last index of s + 1
+        dp[length] = True
 
-        for i in range(length -1, -1, -1): # checking from last
-            for word in wordDict: # comparing each word 
-                if (i + len(word)) <= length and s[i : i + len(word)] == word: # <= as dp[length] is also set
-            # if higher indexes are true that means it can be used to evaluate lower indexes
-                    dp[i] = dp[i + len(word)] 
-                
-                if dp[i]: # if true
+        for i in range(length - 1, -1, -1):
+            for word in wordDict:
+                if dp[i]:
                     break
+                if (i + len(word) <= length) and s[i: i + len(word)] == word:
+                    dp[i] = dp[i + len(word)]
         
         return dp[0]
