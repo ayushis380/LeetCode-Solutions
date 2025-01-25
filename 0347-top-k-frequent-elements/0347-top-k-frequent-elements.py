781eb1fa95d4a76a364e62f9aaa567308386bd44
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-01-24 20:44:40 -0500

    Time: 9 ms (30.99%), Space: 23.5 MB (25.78%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,18 +1,16 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = Counter(nums)
-        heap, topk = [], []
-        heapq.heapify(heap)
+        # arr = [[]] * (len(nums) + 1) # creates a list where each element is a reference to the same list object, so when u update any list it will update all
+        arr = [[] for _ in range(len(nums) + 1)]
+        count = Counter(nums)
+        topk = []
 
-        for key, count in freq.items():
-            heapq.heappush(heap, (count, key))
+        for key, freq in count.items():
+            arr[freq].append(key)
 
-            if len(heap) > k:
-                heapq.heappop(heap)
+        for i in range(len(arr) - 1, 0, -1):
+            for val in arr[i]:
+                topk.append(val)
+                if len(topk) == k:
+                    return topk
         
-        while heap:
-            count, key = heapq.heappop(heap)
-            topk.append(key)
-        
-        return topk
-
