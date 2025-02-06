648e8970918a20f65ec64d77ce7fa682ad4efa42
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-02-06 10:24:45 -0500

    Time: 10 ms (28.27%), Space: 24.4 MB (6.35%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,16 +1,18 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # arr = [[]] * (len(nums) + 1) # creates a list where each element is a reference to the same list object, so when u update any list it will update all
-        arr = [[] for _ in range(len(nums) + 1)]
-        count = Counter(nums)
+        freq = Counter(nums)
         topk = []
+        arr = [[] for i in range(len(nums) + 1)] # if any number appears that means it can't have 0 as count, so need an extra array at the end
+        # mapping frequency of n to the index of arr
 
-        for key, freq in count.items():
-            arr[freq].append(key)
-
-        for i in range(len(arr) - 1, 0, -1):
-            for val in arr[i]:
-                topk.append(val)
-                if len(topk) == k:
-                    return topk
+        for n, count in freq.items():
+            arr[count].append(n)
         
+        print(arr)
+        for i in range(len(arr) - 1, 0, -1):
+            for n in arr[i]:
+                topk.append(n)
+            
+            if len(topk) == k:
+                return topk
+
