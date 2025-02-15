db4ee21b6d9f4d4621eed2d0363ae11522290520
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-02-15 18:33:29 -0500

    Time: 7 ms (81.16%), Space: 18.1 MB (57.34%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -5,13 +5,12 @@ class Solution:
         while low < high:
             while low < high and not s[low].isalnum():
                 low += 1
-            
             while low < high and not s[high].isalnum():
                 high -= 1
-            
             if s[low].lower() != s[high].lower():
                 return False
+            
             low += 1
             high -= 1
-
+        
         return True
