4ccc9626a9c9d52310178e6f7b911a957aab7c8c
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-02-15 19:10:48 -0500

    Time: 64 ms (43.57%), Space: 18 MB (62.58%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,22 +1,21 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
         low, high = 0, len(s) - 1
-        flag = False
 
-        def check(low, high):
-            nonlocal flag
-            if low > high:
-                return True
-
-            res = False
-            if s[low] == s[high]:
-                res = check(low + 1, high - 1)
-            elif flag == True:
-                return False
-            elif flag == False:
-                flag = True
-                res = check(low + 1, high) or check(low, high - 1)
+        def is_Palindrome(low, high):
+            while low < high:
+                if s[low] != s[high]:
+                    return False
+                low += 1
+                high -= 1
             
-            return res
+            return True
         
-        return check(low, high)
+        while low < high:
+            if s[low] != s[high]:
+                return is_Palindrome(low + 1, high) or is_Palindrome(low, high - 1)
+            
+            low += 1
+            high -= 1
+        
+        return True
