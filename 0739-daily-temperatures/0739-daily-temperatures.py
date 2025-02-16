125b4b95723eb2587f1ec07a6b3b1fd2fecd9b92
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-02-16 18:54:08 -0500

    Time: 119 ms (49.45%), Space: 33 MB (42.05%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,14 +1,13 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        warmer = [0] * len(temperatures)
         stack = []
-        result = [0] * len(temperatures)
 
-        # monoton decreasing stack
-        for i, tem in enumerate(temperatures):
-            while stack and stack[-1][1] <= tem:
-                i_pop, tem_pop = stack.pop()
-                result[i_pop] = i - i_pop
-            
-            stack.append((i, tem))
+        for i, t in enumerate(temperatures):
+            while stack and stack[-1][1] < t:
+                i_old, t_old = stack.pop()
+                warmer[i_old] = i - i_old
 
-        return result
+            stack.append((i, t))
+        
+        return warmer
