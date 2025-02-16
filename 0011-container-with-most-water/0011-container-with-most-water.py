22c0500b04e6f7b5d3a3d0c5ee44c6bebe4521fa
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-02-15 21:00:59 -0500

    Time: 100 ms (63.32%), Space: 28.5 MB (67.25%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -4,15 +4,13 @@ class Solution:
         water = 0
 
         while low < high:
-            minht = min(height[low], height[high])
-            store = (high - low) * minht
-            water = max(water, store)
+            minw = min(height[low], height[high])
+            cur_water = (high - low) * minw
+            water = max(water, cur_water)
 
             if height[low] < height[high]:
                 low += 1
             else:
                 high -= 1
-        
-        return water
             
-
+        return water
