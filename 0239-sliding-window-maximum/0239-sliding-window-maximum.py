b4b55ac661c764d190addbdc858ae034b8417edb
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-02-16 18:09:44 -0500

    Time: 185 ms (43.38%), Space: 31 MB (79.48%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,25 +1,26 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-    # To start appending maximum values to the output list only after the window is fully formed. If this check isn't in place, you'd incorrectly compute maximums before the window contains \U0001d458 elements.  
-    # once a k window is formed, it will continue but then we need to adjust the left pointer
+        queue = deque() # indexes, mono decreasing values
         output = []
-        q = deque() # stores indices of nums and is monotonically decreasing(in regards with nums values)
         l, r = 0, 0
 
         while r < len(nums):
-            while q and nums[q[-1]] < nums[r]: 
-                q.pop() # pop all smaller values than the value at index r
+            while queue and nums[queue[-1]] < nums[r]:
+                queue.pop()
             
-            q.append(r)
+            queue.append(r)
 
-            if l > q[0]: # happens when old window's index of max value is still in the queue
-            # eg 1 3 1 2 0 5, k = 3, when l = 2, q still stores index of value 3 
-                q.popleft() # l is the start of the window, we need to update the window 
+            if l > queue[0]: # if start of window has moved ahead of queue's top value(index), that means queue needs to be updated as it has stale index entries 
+                queue.popleft()
             
-            if (r + 1) >= k: # r + 1 as k denotes size and r is an index, eg at r = 2 we are processing the third element, after a point this will always be true
-                output.append(nums[q[0]])
-                l += 1 # only increment it when we have updated the max value of curr window
+            if (r + 1) >= k:
+                output.append(nums[queue[0]])
+                l += 1
             
-            r += 1 # r is for the end index of window
+            r += 1
         
         return output
+
+
+
+
