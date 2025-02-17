abd11ea2bc33dded64929e6b9d3e722f97917757
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-02-17 18:41:13 -0500

    Time: 0 ms (100%), Space: 17.8 MB (34.43%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -5,12 +5,14 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        left = ListNode(0, head)
-        right = head
+        # Doing in one pass : maintain n nodes btwn left and right
+        # Remove the (L−n+1) th node from the beginning in the list
+        dummy = ListNode(0, head)
+        left = dummy
+        right = dummy
 
-        while n > 0 and right:
+        for i in range(n + 1): # now right is at dummy, so should take n+ 1 steps
             right = right.next
-            n -= 1
         
         while right:
             right = right.next
@@ -18,4 +20,5 @@ class Solution:
         
         left.next = left.next.next
 
-        return head
+        return dummy.next
+        # return head # If the first node (or any node) is removed, head remains unchanged in memory, which might lead to incorrect results.
