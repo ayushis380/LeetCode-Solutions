9fb14f669ab154b71cb6d90d8dbcbed28b02e5a5
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-02-16 23:52:16 -0500

    Time: 3 ms (53.52%), Space: 17.9 MB (83.59%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,32 +1,28 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-    # O(Log(Min(M,N)))
-
         A, B = nums1, nums2
         if len(A) > len(B):
             A, B = B, A
         
-        l, r = 0, len(A) - 1
         total = len(A) + len(B)
-        half = total // 2 # if we just find the half of the elements of the final array, we can find the median - we dont need to combine the full array
+        half = total//2
+        low, high = 0, len(A) - 1
 
         while True:
-            i = (l + r) //2  # mid value
+            i = low + (high - low)//2
             j = half - i - 2
 
-            Aleft = A[i] if i >= 0 else float("-inf")
-            Aright = A[i+1] if (i+1) < len(A) else float("inf")
+            A_left = A[i] if i >= 0 else float("-inf")
+            A_right = A[i+1] if (i + 1) < len(A) else float("inf")
 
-            Bleft = B[j] if j >= 0 else float("-inf")
-            Bright = B[j+1] if (j+1) < len(B) else float("inf")
+            B_left = B[j] if j >= 0 else float("-inf")
+            B_right = B[j+1] if (j + 1) < len(B) else float("inf")
 
-            if Aleft <= Bright and Bleft <= Aright:
-                if total % 2:
-                    return min(Aright, Bright)
-                return (max(Aleft, Bleft) + min(Aright, Bright))/2
-            elif Aleft > Bright:
-                r = i - 1
+            if A_left <= B_right and B_left <= A_right:
+                if total%2:
+                    return min(A_right, B_right)
+                return (max(A_left, B_left) + min(A_right, B_right)) /2
+            elif A_left > B_right:
+                high = i - 1
             else:
-                l = i + 1
-
-
+                low = i + 1
