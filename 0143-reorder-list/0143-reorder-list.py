db04c48c54f33238cdf55692d54a2229d807ca0c
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-02-17 13:57:17 -0500

    Time: 4 ms (37.29%), Space: 23.4 MB (12.37%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -5,26 +5,26 @@
 #         self.next = next
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-        # find middle
-        fast, slow = head.next, head
+        slow, fast = head, head.next
+        
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
 
-        # reverse second half
-        second = slow.next # start of second half
-        prev = slow.next = None # first half ends at slow so slow.next is set to None
+        cur = slow.next
+        prev = slow.next = None
 
-        while second:
-            tmp = second.next
-            second.next = prev
-            prev = second
-            second = tmp
+        while cur:
+            tmp = cur.next
+            cur.next = prev
+            prev = cur
+            cur = tmp
         
-        # merge first and second(reversed) half
-        first, second = head, prev # prev becomes the head of reversed second half
-        while second: # first and second will be of same size when even, second will be shorter when odd
-            tmp1, tmp2 = first.next, second.next # storing next vals before we break links
+        first, second = head, prev
+        while second:
+            t1, t2 = first.next, second.next
             first.next = second
-            second.next = tmp1
-            first, second = tmp1, tmp2
+            second.next = t1
+            first, second = t1, t2
+
+        
