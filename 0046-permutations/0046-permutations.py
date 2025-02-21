18682587be1916ac59339254117f49ec424dd7e2
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-02-21 10:08:32 -0500

    Time: 0 ms (100%), Space: 18 MB (56.39%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,13 +1,15 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         if len(nums) == 0:
-            return [[]] 
+            return [[]]
         
-        perms = self.permute(nums[1:]) # nums[0] will be inserted in this 2D list
-        res = []
-        for p in perms: # for all perms we need to insert nums[0] in p's range 
-            for i in range(len(p) +1): # value can be inserted at end - so +1 
+        perms = self.permute(nums[1:])
+        output = []
+        
+        for p in perms:
+            for i in range(len(p) + 1):
                 p_copy = p.copy()
                 p_copy.insert(i, nums[0])
-                res.append(p_copy)
-        return res
+                output.append(p_copy)
+        
+        return output
