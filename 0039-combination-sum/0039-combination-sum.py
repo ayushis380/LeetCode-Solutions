594e6b874896cb6d18aa047e989f27ba9de35e78
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-02-20 21:34:59 -0500

    Time: 11 ms (60.71%), Space: 17.7 MB (87.6%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,19 +1,23 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = []
-        def dfs(i, curr, total):
-            if total == target:
-                res.append(curr.copy())
+        combinations = []
+        path = []
+
+        def dfs(totalsum, i):
+            if totalsum == target:
+                combinations.append(path.copy())
                 return
-            if i >= len(candidates) or total > target:
+
+            if totalsum > target or i >= len(candidates):
                 return
-            # include 
-            curr.append(candidates[i])
-            dfs(i, curr, total + candidates[i])
             
-            # not include 
-            curr.pop()
-            dfs(i+1, curr, total)
+            # here totalsum is unchanged, we are adding a value to it
+            path.append(candidates[i])
+            dfs(totalsum + candidates[i], i)
+
+            path.pop()
+            dfs(totalsum, i+1) # as totalsum is unchanged so need to substract candidates[i] from it
         
-        dfs(0, [], 0)
-        return res
+        dfs(0,0)
+
+        return combinations
