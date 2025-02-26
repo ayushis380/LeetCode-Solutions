4ccaa73119db66d0384d4f7faa4bbe12970b2bcf
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-02-26 18:26:38 -0500

    Time: 4 ms (76.59%), Space: 21.6 MB (73.59%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,29 +3,28 @@ class Solution:
         rows, cols = len(board), len(board[0])
 
         def dfs(r, c):
-            if r >= rows or r < 0 or c >= cols or c < 0 or board[r][c] == "X" or board[r][c] == "V":
+            if r >= rows or r < 0 or c >= cols or c < 0 or board[r][c] != "O":
                 return
             
-            board[r][c] = "V"
-
-            dfs(r+1, c)
-            dfs(r-1, c)
-            dfs(r, c-1)
-            dfs(r, c+1)
-            return 
-
-        for c in range(cols):
-            dfs(0, c)
-            dfs(rows-1, c)
+            board[r][c] = "T"
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
         
         for r in range(rows):
             dfs(r, 0)
-            dfs(r, cols-1)
+            dfs(r, cols - 1)
+        
+        for c in range(cols):
+            dfs(0, c)
+            dfs(rows - 1, c)
         
         for r in range(rows):
             for c in range(cols):
                 if board[r][c] == "O":
                     board[r][c] = "X"
-                if board[r][c] == "V":
+                if board[r][c] == "T":
                     board[r][c] = "O"
-                
+
+        
