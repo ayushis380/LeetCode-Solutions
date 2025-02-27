cb9d13b71a6bda714a29e7b6be55c080ee2dab4a
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-02-27 12:41:41 -0500

    Time: 250 ms (76.07%), Space: 28.2 MB (85.26%) - LeetHub

--- 3371-identify-the-largest-outlier-in-an-array/3371-identify-the-largest-outlier-in-an-array.py
+++ 3371-identify-the-largest-outlier-in-an-array/3371-identify-the-largest-outlier-in-an-array.py
@@ -1,22 +1,15 @@
 class Solution:
     def getLargestOutlier(self, nums: List[int]) -> int:
-        # finding a value val, for which outlier = total - 2 * val 
-        # total = sum of n-2 elem + sum + outlier
-    
-    # eg [2,3,5,10] here when val = 5 we can find the outlier 
-    # as it satisfies the cond
-        total_sum = sum(nums)
+        total = sum(nums)
         outlier = float("-inf")
-
         count = Counter(nums)
 
         for val in nums:
-            potential_outlier = total_sum - 2 * val
-    
-    # [1,1,1,1,1,5,5] here val and outlier are same
-    # so the count is also greater than 1 
-            if potential_outlier in count:
-                if potential_outlier != val or count[val] > 1:
-                    outlier = max(outlier, potential_outlier)
+            potential = total - 2 * val
+
+            if potential in count:
+                if potential != val or count[val] > 1:
+                    outlier = max(outlier, potential)
         
         return outlier
+
