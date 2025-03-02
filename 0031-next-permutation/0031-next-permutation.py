9d93959c4572f8f9b75d5a519f3c345e4c881bd2
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-03-01 22:18:14 -0500

    Time: 0 ms (100%), Space: 17.6 MB (85.74%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,27 +1,21 @@
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
+    # 1. find the lowest value L from end of nums - as we need next greater value so this need to swapped with something big
+    # 2. swap L with next greater value than it from end - once placed the right values to it can be sorted to find next perm
+    # 3. sort from L's index +1 till end - 
+
         n = len(nums)
         i = n - 2
 
-        # find the pivot, the value smaller than its right half
-        # Step 1: Find the first decreasing element from the right
         while i >= 0 and nums[i] >= nums[i+1]:
             i -= 1
         
-        # Step 2: If such an element exists, find the next larger element(from end) to swap
-        if i >= 0:
-            j = n - 1
+        if i >=0:
+            j = n -1
             while nums[j] <= nums[i]:
                 j -= 1
             nums[i], nums[j] = nums[j], nums[i]
-
-        # Step 3: Reverse the suffix
-        # In the first step we already went till the nums was in increasing order
-        # so sorting after ith index will give the next greater value
-        left, right = i + 1, n - 1
-        while left < right:
-            nums[left], nums[right] = nums[right], nums[left]
-            left += 1
-            right -= 1
+        
+        nums[i+1:] = sorted(nums[i+1:])
 
         
