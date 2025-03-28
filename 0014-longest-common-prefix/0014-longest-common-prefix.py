4752af0d13a83034ae0872b65c6c71aa069cad15
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-03-28 05:21:25 -0400

    Time: 3 ms (28.2%), Space: 17.9 MB (30.74%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -5,14 +5,14 @@ class Solution:
 
         if minlen == 0:
             return ""
-
+        
         for i in range(minlen):
-            char = strs[0][i] # first string in strs
+            ch = strs[0][i]
 
-            for st in strs:
-                if st[i] != char: # compare the char with all other strings for that index
+            for s in strs:
+                if ch != s[i]:
                     return common
             
-            common += char
+            common += ch
         
         return common
