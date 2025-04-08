bebac754ae229d7f6bb7945e4cf9d7dde3992e92
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-04-08 14:05:38 -0400

    Time: 11 ms (52.64%), Space: 18.5 MB (17.72%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,18 +1,15 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        maxproduct = nums[0] # not 0 as nums contains -ve values as well
-        curMin, curMax = 1, 1
+        maxprod = nums[0]
+        curmax, curmin = nums[0], nums[0]
 
-        # array has both +ve and -ve numbers
-        # curMax and curMin are max and min till that index
-        # values can be zero as well which makes curMax and curMin to 0 - a breakpoint but
-        # the third value in max and min equation handles it
-        
-        for val in nums:
-            temp = val * curMax # curMax is overwritten 
-            curMax = max(val * curMax, val * curMin, val) # [-1, 8] here 8 is picked
-            curMin = min(temp, val * curMin, val) # [-1, -8] here -8 should be taken
+        for i in range(1, len(nums)):
+            val = nums[i]
             
-            maxproduct = max(maxproduct, curMax)
+            temp = curmax
+            curmax = max(val * curmax, val * curmin, val)
+            curmin = min(val * curmin, temp * val, val)
+
+            maxprod = max(maxprod, curmax)
         
-        return maxproduct
+        return maxprod
