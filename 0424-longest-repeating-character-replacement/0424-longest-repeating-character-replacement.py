f9c4d39ab2c47c24072d85d7a09ca6501781b152
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-04-11 04:14:36 -0400

    Time: 114 ms (81.05%), Space: 18 MB (87.02%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,15 +1,17 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        res = 0
-        freq = {}
+        count = defaultdict(int)
+        maxfreq = 0
+        maxlen = 0
         start = 0
-        maxfreq = 0 # once we have a max value we wont change it if we get a less freq char as it maximise res we need to maximize maxfreq, 
-        for end in range(len(s)):
-            freq[s[end]] = freq.get(s[end], 0) + 1
-            maxfreq = max(maxfreq, freq[s[end]])
-            while (end-start+1) - maxfreq > k:
-                freq[s[start]] -= 1
-                start +=1
 
-            res = max(res, end-start+1)
-        return res
+        for end in range(len(s)):
+            count[s[end]] += 1
+            maxfreq = max(maxfreq, count[s[end]])
+            if (end - start + 1) - maxfreq > k:
+                count[s[start]] -= 1
+                start += 1
+            
+            maxlen = max(maxlen, end - start + 1)
+        
+        return maxlen
