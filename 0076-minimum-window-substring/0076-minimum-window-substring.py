61d0a4c7ec7850e2ef73a2e87504d37db838aafb
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-04-11 05:03:50 -0400

    Time: 91 ms (49.95%), Space: 18.2 MB (56.26%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -3,17 +3,18 @@ class Solution:
         if len(t) > len(s):
             return ""
         
-        result, minlen = [], float("inf")
-        s_count = defaultdict(int)
-        t_count = Counter(t)
-        need, have = len(t_count), 0
+        count_s = Counter()
+        count_t = Counter(t)
+        have, need = 0, len(count_t)
         start = 0
+        l, r = -1, -1 
+        minlen = float("inf")
 
         for end in range(len(s)):
             ch = s[end]
-            s_count[ch] += 1
+            count_s[ch] += 1
 
-            if ch in t_count and t_count[ch] == s_count[ch]:
+            if ch in count_t and count_t[ch] == count_s[ch]: # if count_s[ch] >= count_t[ch], have not increased as it would have already been increased when they were equal - have shoulld be updated per new character
                 have += 1
             
             while have == need:
@@ -21,22 +22,14 @@ class Solution:
                 
                 if minlen > length:
                     minlen = length
-                    result = s[start: end + 1]
-                
-                ch_start = s[start]
-                s_count[ch_start] -= 1
+                    l, r = start, end + 1
 
-                # only decrement when count of ch in s is less than the count in t
-                # it makes the window invalid
-                # if we have count greater than its fine
-                if ch_start in t_count and s_count[ch_start] < t_count[ch_start]:
+                ch_st = s[start]
+                count_s[ch_st] -= 1 # have == need so decrease the window to look for a smaller window which satisfies this condition
+
+                if ch_st in count_t and count_s[ch_st] < count_t[ch_st]:
                     have -= 1
                 
                 start += 1
         
-        return result if result != [] else ""
-            
-            
-            
-
-        
+        return s[l : r]
