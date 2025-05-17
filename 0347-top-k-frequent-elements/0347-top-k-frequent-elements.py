d2f3ef88b0727976346f0fce6063c547164eaab4
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-05-17 17:10:37 -0400

    Time: 9 ms (28.59%), Space: 24.4 MB (7.65%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,19 +1,20 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # O(N) TC better than heap (N logk)
         freq = Counter(nums)
-        heap = []
         topk = []
+        arr = [[] for i in range(len(nums) + 1)]
 
         for key, count in freq.items():
-            heapq.heappush(heap, (count, key))
+            arr[count].append(key)
+        
+        for i in range(len(arr) -1, 0, -1):
+            for val in arr[i]:
+                topk.append(val)
+            
+            if len(topk) == k:
+                return topk
 
-            if len(heap) > k:
-                heapq.heappop(heap)
-        
-        while heap:
-            count, key = heapq.heappop(heap)
-            topk.append(key)
-        
-        return topk
-        
+            
 
+        
