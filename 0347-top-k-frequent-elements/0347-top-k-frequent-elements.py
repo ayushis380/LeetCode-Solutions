dd79f0e1ab8873ef60444c745c46ca4ff1089e1c
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-05-17 00:04:27 -0400

    Time: 5 ms (61.28%), Space: 21.1 MB (80.51%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,18 +1,19 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         freq = Counter(nums)
+        heap = []
         topk = []
-        arr = [[] for i in range(len(nums) + 1)] # if any number appears that means it can't have 0 as count, so need an extra array at the end
-        # mapping frequency of n to the index of arr
 
-        for n, count in freq.items():
-            arr[count].append(n)
+        for key, count in freq.items():
+            heapq.heappush(heap, (count, key))
+
+            if len(heap) > k:
+                heapq.heappop(heap)
         
-        print(arr)
-        for i in range(len(arr) - 1, 0, -1):
-            for n in arr[i]:
-                topk.append(n)
-            
-            if len(topk) == k:
-                return topk
+        while heap:
+            count, key = heapq.heappop(heap)
+            topk.append(key)
+        
+        return topk
+        
 
