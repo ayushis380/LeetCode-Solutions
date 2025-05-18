280a56ee8e22eff41b5fa884eed79c421b5c13ec
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-05-17 20:54:44 -0400

    Time: 7 ms (89.09%), Space: 19.2 MB (93.29%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,19 +1,17 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-# we know the current bar (pointed by left) will never be affected by any bar on the right of right. This is because rmax is already greater than lmax, and future bars can only reduce the impact of rmax on water calculations.
-# even if the values are smaller in the middle it doesnt matter as we look for max values from both sides and then pick the minimum
-        left, right = 0, len(height) - 1
+        low, high = 0, len(height) - 1
+        lmax, rmax = height[low], height[high]
         water = 0
-        lmax, rmax = height[left], height[right]
-    
-        while left < right:
+
+        while low < high:
             if lmax < rmax:
-                left += 1
-                lmax = max(lmax, height[left])
-                water += lmax - height[left]
+                low += 1 # think of lmax as the smaller value as compared to rmax - smaller value decides the amount of water stored 
+                lmax = max(lmax, height[low]) # comparing the max on left side to the height where we are at currently
+                water += lmax - height[low]
             else:
-                right -= 1
-                rmax = max(rmax, height[right])
-                water += rmax - height[right]
+                high -= 1
+                rmax = max(rmax, height[high])
+                water += rmax - height[high]
         
         return water
