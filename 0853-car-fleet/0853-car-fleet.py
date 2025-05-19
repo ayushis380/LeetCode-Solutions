0773bd6f8df9d4eced52ed2969461cf1efebbe53
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-05-18 22:20:39 -0400

    Time: 169 ms (49.6%), Space: 37.5 MB (83.15%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,22 +1,15 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        time = [(p, s) for p, s in zip(position, speed)]
-        time.sort(reverse=True) # sorted on basis of position in reverse
-
+        arr = [(pos, sp) for pos, sp in zip(position, speed)]
+        arr.sort(reverse=True)
         stack = []
 
-        for p, s in time:
-        # car cannot pass another car so cars in middle can have different speeds 
-        # so it is sorted as per positions from last
-            stack.append((target - p)/s) # positions closer to target are added first 
+        for p, s in arr:
+            t = (target - p) / s
 
-        # ind = -1 has the newly added value, ind = -2 the old value
-        # if new value is less than old value it means car at -1 is faster than -2 and it will catch up
-        # as cars can't go ahead if they are faster, new value is popped
-        # car will move at the speed of old value
+            stack.append(t)
 
-            if len(stack) >= 2 and stack[-1] <= stack[-2]: 
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
                 stack.pop()
-        
-        return len(stack) # stack has all the fleet speeds (slower speeds as faster cars catch up to slower ones)
 
+        return len(stack) 
