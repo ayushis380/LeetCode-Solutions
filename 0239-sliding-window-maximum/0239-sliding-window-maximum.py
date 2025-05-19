0e4f4fb591052217f237cd5797ceefb8766566b7
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-05-18 21:39:51 -0400

    Time: 171 ms (54.11%), Space: 30.9 MB (86.63%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,26 +1,28 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        queue = deque() # indexes, mono decreasing values
-        output = []
+        queue = deque() # monotonic decreasing 
         l, r = 0, 0
+        result = []
+    # performing popleft and pop on queue
+    # both pop values from different ends
 
         while r < len(nums):
-            while queue and nums[queue[-1]] < nums[r]:
-                queue.pop()
+            while queue and nums[r] > nums[queue[-1]]:
+                queue.pop() # removing the top element - on rightmost
             
             queue.append(r)
 
-            if l > queue[0]: # if start of window has moved ahead of queue's top value(index), that means queue needs to be updated as it has stale index entries 
-                queue.popleft()
-            
-            if (r + 1) >= k:
-                output.append(nums[queue[0]])
+            # shift in window 
+            if l > queue[0]: # means we have already added this nums[queue[0]] value to output and its ok to pop it
+                queue.popleft() # removing first value - on leftmost
+
+            if r >= k - 1:
+                # see we are not popping value from queue 
+                # so to make sure the values are from l - r window, we do a check l > queue[0]
+                result.append(nums[queue[0]]) # first value is the largest
                 l += 1
             
             r += 1
         
-        return output
-
-
-
+        return result 
 
