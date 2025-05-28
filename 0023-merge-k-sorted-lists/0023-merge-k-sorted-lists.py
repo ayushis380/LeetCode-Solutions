86a0ecbda452535532de4fc677247c6b40d5cd68
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-05-28 19:35:50 -0400

    Time: 19 ms (27.54%), Space: 20.2 MB (70.19%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,21 +5,39 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        dummy = ListNode(0)
-        cur = dummy
-        heap = []
+        length = len(lists)
+        interval = 1
 
-        for i, ls in enumerate(lists):
-            if ls:
-                heapq.heappush(heap, (ls.val, i, ls))
+# interval * 2 is used to control the gap between pairs being merged, and it doubles each iteration to reduce the number of lists logarithmically
+# i jumps by interval * 2 because each merge operation combines two intervals of length interval
 
-        while heap:
-            val, i, ls = heapq.heappop(heap)
-            node = ListNode(val)
-            cur.next = node
-            cur = node
-
-            if ls.next:
-                heapq.heappush(heap, (ls.next.val, i, ls.next))
+        while interval < length:
+            for i in range(0, length - interval, interval * 2):
+                print("i is ", i)
+                lists[i] = self.mergeTwoLists(lists[i], lists[i+interval])
+            
+            interval *= 2 # less steps in next iteration 
         
-        return dummy.next
+        return lists[0] if length > 0 else None
+    
+    def mergeTwoLists(self, l1, l2):
+        head = ListNode(0)
+        cur = head 
+# not creating new nodes in this approach - its just placing the pointers in right way
+
+        while l1 and l2:
+            if l1.val > l2.val:
+                cur.next = l2
+                l2 = l2.next
+            else:
+                cur.next = l1
+                l1 = l1.next
+            
+            cur = cur.next
+        
+        if l1:
+            cur.next = l1
+        if l2:
+            cur.next = l2
+        
+        return head.next
