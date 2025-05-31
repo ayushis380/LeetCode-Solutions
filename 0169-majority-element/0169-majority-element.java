d1225552aa2d268d358522fce2ec4c747915be83
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-05-31 11:19:08 -0400

    Time: 1 ms (99.84%), Space: 53.1 MB (59.6%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,16 +1,19 @@
 class Solution {
     public int majorityElement(int[] nums) {
-      int el = nums[0];
-      int count = 0;
-      for(int i = 0; i< nums.length; i++){
-        if(count == 0)
-            el = nums[i];
-        if(el == nums[i])
-            count++;
-        else {
-            count--;
+        // Moore's voting algo
+        int count = 0, el =0;
+        for(int i=0; i< nums.length; i++){
+            if(count == 0){ // at this point in array it means 
+            // the element el's count is countered negative by other elements which are not equal to el
+            // this means that el's count will not be > n/2 as now its n/2; n being length till that point
+                count = 1;
+                el = nums[i];
+            } else if(nums[i] == el)
+                count++;
+            else
+                count--;
         }
-      }
-      return el;  
+        return el; // if majority element exists then el will be majority 
+        // if a majority element doesnt exist then check count(el) > n/2 - if true then return el otherwise -1 
     }
 }
