e74adbffaddf2b28837b319962c721dd4e40b1b3
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-05-31 11:07:50 -0400

    Time: 0 ms (100%), Space: 17.7 MB (52.39%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,13 +1,12 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        # order of elements doesnt matter
-        l, r = 0, len(nums)
+        low, high = 0, len(nums)
 
-        while l < r:
-            if nums[l] == val:
-                nums[l] = nums[r - 1]
-                r -= 1 # when no vals are left, r will reach the point from ehere all vals arre stored till end
+        while low < high:
+            if nums[low] == val:
+                nums[low] = nums[high - 1]
+                high -= 1
             else:
-                l += 1
+                low += 1
         
-        return r # r index is from where not required values start, we need length so r is returned
+        return high
