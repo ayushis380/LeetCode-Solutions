d5132d170ae94c9222f7862446c06aad9ca587ed
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-06-02 19:30:50 -0400

    Time: 0 ms (100%), Space: 17.7 MB (73.45%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -1,26 +1,19 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        # we don't need to compare every word to all of the words to its right
-        # just compare each pair of adjacent words
+        ord_map = {}
 
-        order_map = {}
-        for index, ch in enumerate(order):
-            order_map[ch] = index
+        for i, ch in enumerate(order):
+            ord_map[ch] = i
         
-        for i in range(len(words) - 1):
+        for i in range(len(words) - 1): # compare i and i+1 word, if all follow the rule then all are in order 
             for j in range(len(words[i])):
-# If we do not find a mismatch letter between words[i] and words[i + 1],
-# we need to examine the case when words are like ("apple", "app").
+                
                 if j >= len(words[i+1]):
                     return False
-
-# if we find the first different character and they are sorted,
-# then there's no need to check remaining letters              
-                if words[i][j] != words[i+1][j]:
-                    if order_map[words[i][j]] > order_map[words[i+1][j]]:
-                        return False
                 
+                if words[i][j] != words[i+1][j]:
+                    if ord_map[words[i][j]] > ord_map[words[i+1][j]]:
+                        return False
                     break
-
+        
         return True
-
