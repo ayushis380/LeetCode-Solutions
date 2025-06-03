82da45856e665432b28f041c09b2a4402b018356
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-06-03 19:49:46 -0400

    Time: 7 ms (46.5%), Space: 25.7 MB (21.66%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,21 +1,20 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        length = len(nums)
-        k %= length # within bound
+       i = 0
+       n = len(nums)
+       k %= n
 
-        start, count = 0, 0
-        # cyclic replacement
-    # start at 0, now 0 % k = 0, in each iter all indexes with i % k = 0 will be placed correctly
-    # then start is increased and set for all other indexes
-        while count < length:
-            current, prev = start, nums[start]
-            while True:
-                new_idx = (current + k) % length # new index 
-                nums[new_idx], prev = prev, nums[new_idx] # swap with previous value
-                current = new_idx # go to new index
-                count += 1 # only swaps required = length
+       self.reverse(0, nums, n-1) # full reverse
+       self.reverse(0, nums, k-1) # k elements reversed
+       self.reverse(k, nums, n-1)# n -k elements from end reversed 
 
-                if current == start: # reached from where we started
-                    break
-            
-            start += 1 # go to next index and place values
+    #    nums.reverse()
+    #    nums[:k] = reversed(nums[:k])
+    
+    def reverse(self, start, nums, end):
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
+
+        
