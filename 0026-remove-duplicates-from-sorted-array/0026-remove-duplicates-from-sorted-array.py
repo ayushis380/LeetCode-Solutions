a60b0714b6ab20caae22358cfbd00dce9dafc19c
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-06-03 19:16:21 -0400

    Time: 0 ms (100%), Space: 18.8 MB (94.54%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -2,9 +2,9 @@ class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         i = 0
 
-        for j in range(len(nums)): 
-            if nums[i] != nums[j]: # go till they dont match
-                i += 1 # increase i to assign the unmatched value at index i, we need all unique values, which is at index i 
+        for j in range(len(nums)):
+            if nums[i] != nums[j]:
+                i += 1
                 nums[i] = nums[j]
         
-        return i + 1 # need length 
+        return i + 1
