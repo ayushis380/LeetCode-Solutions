ee877f3d15637e43765aca04209c98845d3816c8
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-06-03 20:14:33 -0400

    Time: 15 ms (16.05%), Space: 25.7 MB (47.61%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,21 +1,31 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         length = len(nums)
-        k %= length # within bound
+        k %= length
 
         start, count = 0, 0
-        # cyclic replacement
-    # start at 0, now 0 % k = 0, in each iter all indexes with i % k = 0 will be placed correctly
-    # then start is increased and set for all other indexes
-        while count < length:
-            current, prev = start, nums[start]
-            while True:
-                new_idx = (current + k) % length # new index 
-                nums[new_idx], prev = prev, nums[new_idx] # swap with previous value
-                current = new_idx # go to new index
-                count += 1 # only swaps required = length
 
-                if current == start: # reached from where we started
+# the total count of numbers exclusive numbers placed at their correct position will be k× n / k = n
+
+        while count < length: 
+            current_idx, prev_val = start, nums[start]
+            while True:
+                new_idx = (current_idx + k) % length
+        # place the old value at new index and keep copy of this index value for next iteration
+                nums[new_idx], prev_val = prev_val, nums[new_idx] 
+                # print("new state of nums: ", nums)
+                current_idx = new_idx
+                count += 1
+
+                if current_idx == start: # means placed all values at required position for this value of start 
                     break
             
-            start += 1 # go to next index and place values
+            start += 1
+
+# [1,2,3,4,5,6] k = 2
+# new state of nums:  [1, 2, 1, 4, 5, 6]
+# new state of nums:  [1, 2, 1, 4, 3, 6]
+# new state of nums:  [5, 2, 1, 4, 3, 6]
+# new state of nums:  [5, 2, 1, 2, 3, 6]
+# new state of nums:  [5, 2, 1, 2, 3, 4]
+# new state of nums:  [5, 6, 1, 2, 3, 4]
