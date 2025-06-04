1a1a58d791fd4d8f463743e34a4da204518d6a45
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-06-03 22:25:51 -0400

    Time: 6 ms (64.4%), Space: 19 MB (62.34%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -1,15 +1,16 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        heap = []
-        for i in range(len(arr)):
-            diff = abs(arr[i] - x)
-            if len(heap) < k:
-                heapq.heappush(heap, (-diff, i))
-            elif diff < -heap[0][0]:
-                heapq.heappushpop(heap, (-diff, i))
-        
-        result = [arr[p[1]] for p in heap]
-        result.sort()
-        return result
+        # O(n - k) — because we shrink the window n - k times (each time either l or r moves).
+# The array is sorted, which allows us to safely trim the farthest elements from both ends without missing any possible closer elements in between.
+# We're always removing the element that's less optimal (farther from x, or larger in case of a tie).
+# Once the window is size k, the elements inside are the best possible choice.
 
-                 
+        left, right = 0, len(arr) - 1
+
+        while right - left >= k:
+            if abs(x - arr[left]) <= abs(x - arr[right]): # equal cond to right as its sorted as left value is given priority 
+                right -= 1
+            else:
+                left += 1
+        
+        return arr[left: right + 1]
