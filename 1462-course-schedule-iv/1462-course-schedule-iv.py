13733960cb3d53f4e136abdfd61913bb58d3d1c2
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-06-04 21:56:36 -0400

    Time: 28 ms (85.04%), Space: 20.7 MB (19.01%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.py
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -1,22 +1,25 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
         adj = [set() for _ in range(numCourses)]
+        preReq = [set() for _ in range(numCourses)]
         indegree = [0] * numCourses
-        isPrereq = [set() for _ in range(numCourses)]
-        
+
         for pre, crs in prerequisites:
             adj[pre].add(crs)
             indegree[crs] += 1
         
-        q = deque([i for i in range(numCourses) if indegree[i] == 0])
+        queue = deque([i for i in range(numCourses) if indegree[i] == 0])
+
+        while queue:
+            node = queue.popleft() # course with no dependencies are popped first 
+
+            for nei in adj[node]:
+                preReq[nei].add(node) # neighbors pre req is node, add that 
+                preReq[nei].update(preReq[node]) # add pre req of node, indirect prereq for nei
+# update() method is used to modify sets by adding elements from other iterables (like lists, tuples, or other sets)
+                
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    queue.append(nei)
         
-        while q:
-            node = q.popleft()
-            for neighbor in adj[node]:
-                isPrereq[neighbor].add(node)
-                isPrereq[neighbor].update(isPrereq[node])
-                indegree[neighbor] -= 1
-                if indegree[neighbor] == 0:
-                    q.append(neighbor)
-        
-        return [u in isPrereq[v] for u, v in queries]
+        return [u in preReq[v] for u, v in queries]
