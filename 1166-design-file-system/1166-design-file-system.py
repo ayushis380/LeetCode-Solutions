2e59db039f3794c6b9644401c68b5c3262a4f610
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-06-06 13:32:39 -0400

    Time: 51 ms (40.92%), Space: 25.5 MB (19.05%) - LeetHub

--- 1166-design-file-system/1166-design-file-system.py
+++ 1166-design-file-system/1166-design-file-system.py
@@ -1,22 +1,50 @@
+class TrieNode:
+    def __init__(self, name):
+        self.map = defaultdict(TrieNode)
+        self.value = -1
+        self.name = name # sub file path name
+
 class FileSystem:
 
     def __init__(self):
-        self.paths = {}
+        self.root = TrieNode("") # start with empty root
 
     def createPath(self, path: str, value: int) -> bool:
-        if path in self.paths:
-            return False
+        # It takes O(T) to add a path to the trie if it contains T components
 
-        parent = path[:path.rfind('/')] # check that the immediate parent path exists — not all previous segments. This is done by finding the last / and checking if the parent path is in the paths dictionary. For path "/a/b", you must ensure "/a" exists
-        print(path.rfind('/')) # gives index of last '/
-        if parent and parent not in self.paths:
+        cur = self.root # all are types of TrieNode 
+        components = path.split("/")
+
+        for i in range(1, len(components)): # go through all
+            name = components[i]
+
+            if name not in cur.map:
+                if i == len(components) - 1: # if last sub path then that means create a new path for it 
+                    cur.map[name] = TrieNode(name) # Adds it to the current node's child map (cur.map) under key name
+                else: 
+                    return False # parent paths doenst exist
+            cur = cur.map[name]
+        
+        if cur.value != -1: # path already present
             return False
         
-        self.paths[path] = value
+        cur.value = value # set the value
         return True
 
     def get(self, path: str) -> int:
-        return self.paths.get(path, -1)
+        # takes O(T) to find a path in the trie if it contains T components.
+        cur = self.root
+        components= path.split("/")
+
+        for i in range(1, len(components)):
+            name = components[i]
+
+            if name not in cur.map:
+                return -1
+            cur = cur.map[name]
+        
+        return cur.value
+        
 
 
 # Your FileSystem object will be instantiated and called as such:
