044b93d2d3dccbb81e834dcce131c67d13a34235
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-06-07 13:13:27 -0400

    Time: 39 ms (78.21%), Space: 20.5 MB (29.42%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
@@ -1,6 +1,7 @@
 class Solution:
     def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
-        jobs = sorted(zip(difficulty, profit))
+        
+        jobs = sorted(zip(difficulty, profit)) # nlog n
 
         max_profit_so_far = 0
         max_profits = []
@@ -13,6 +14,7 @@ class Solution:
             max_profits.append(max_profit_so_far)
         
         for w in worker:
+            # m log n : m workers, log n for binary search
             idx = bisect.bisect_right(difficulties, w) -1 # difficulties has sorted values of difficulty
             if idx >= 0:
                 total_profit += max_profits[idx]
