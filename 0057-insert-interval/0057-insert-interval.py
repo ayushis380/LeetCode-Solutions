a095f44dc50cd6bd4f3a49ca87d41868bcde523f
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-06-08 21:46:37 -0400

    Time: 4 ms (20.69%), Space: 19.8 MB (19.79%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,18 +1,17 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        res = []
-        for i in range(len(intervals)):
-            if newInterval[1] < intervals[i][0]: # end is less than start of ith interval so NI should come before
-                res.append(newInterval)
-                return res + intervals[i:]
-            elif newInterval[0] > intervals[i][1]: # start is greater than end of ith interval so interval will come before
-                res.append(intervals[i])
-            else: # overlapping
-                newInterval = [
-                    min(newInterval[0], intervals[i][0]),
-                    max(newInterval[1], intervals[i][1])
-                ]
-        res.append(newInterval) # we dont merge newInterval till end as it can merge with other intervals
-        return res
+        output = []
 
-                
+        for i in range(len(intervals)):
+            # end of new Interval < start of exiting, so means nI is ending before
+            if newInterval[1] < intervals[i][0]:  
+                output.append(newInterval)
+                return output + intervals[i:]
+            elif newInterval[0] > intervals[i][1]: # start of nI is > end of existing, so nI will come afterwards
+                output.append(intervals[i])
+            else:
+                newInterval = [min(intervals[i][0], newInterval[0]), 
+                max(newInterval[1], intervals[i][1])]
+        
+        output.append(newInterval)
+        return output
