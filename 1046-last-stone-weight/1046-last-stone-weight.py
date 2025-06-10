8dc49b075f473ee51ffafb05b61bc018d0bb5b35
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-06-09 23:34:49 -0400

    Time: 3 ms (9.24%), Space: 17.6 MB (96.72%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,16 +1,17 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        heap = []
+# A Max-Heap is a data structure that can take items, and can remove and return the maximum, with both operations taking O(logN) time. It does this by maintaining the items in a special order (within the array), or as a balanced binary tree
 
-        for s in stones:
-            heapq.heappush(heap, -s)
+        heap = [-st for st in stones]
+        heapq.heapify(heap) # O(N)
 
-        while len(heap) >= 2:
+        while len(heap) > 1:
+            # three O(logN) operations
+#  the three is an ignored constant. This means that we're doing N⋅O(logN)=O(NlogN) operations
             y = -1 * heapq.heappop(heap)
             x = -1 * heapq.heappop(heap)
 
-            if x != y:
-                heapq.heappush(heap, - (y - x))
+            if y != x:
+                heapq.heappush(heap, -1 * (y - x))
         
-        return -heap[0] if heap else 0
-        
+        return -1 * heap[0] if len(heap) else 0
