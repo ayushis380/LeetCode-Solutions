4127f602ed8566e63c795a39cbb234941d837ed1
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-06-11 22:17:30 -0400

    Time: 3 ms (34.06%), Space: 19.3 MB (72.93%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.py
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.py
@@ -6,16 +6,18 @@
 #         self.right = right
 class Solution:
     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        # insert a new node as a child of the leaf
-        # Both tc and sc : O(H), where H is a tree height. That results in O(logN) in the average case, and O(N) in the worst case.
-
         if not root:
-            return TreeNode(val) # when find the next empty child position, return this node
+            return TreeNode(val)
         
-        if val > root.val:
-            # assignment is necessary
-            root.right = self.insertIntoBST(root.right, val)
-        else:
-            root.left = self.insertIntoBST(root.left, val)
-        
-        return root
+        cur = root
+        while True:
+            if val > cur.val:
+                if not cur.right:
+                    cur.right = TreeNode(val)
+                    return root # root is returned, not cur
+                cur = cur.right # keep on checking till we get a child pos to insert
+            else:
+                if not cur.left:
+                    cur.left = TreeNode(val)
+                    return root
+                cur = cur.left
