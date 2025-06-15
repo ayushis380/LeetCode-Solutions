fb307b098523ff6fe733f75e805d4818a415af74
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-06-15 00:04:41 -0400

    Time: 3 ms (76.92%), Space: 17.7 MB (96.23%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -1,23 +1,17 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        dp = {len(days) : 0} # index - > min cost for that index
+        # bottom up
+        dp = [float("inf")] * (len(days) + 1)
+        dp[len(days)] = 0 # base case, imagine when j covers all index till end then it would be at len(days)
+        # 7 8 20 = days, if at day = 7 we buy 30 day ticket then j would go till end of days
 
-        def dfs(i):
-            if i in dp:
-                return dp[i]
+        for i in reversed(range(len(days))): # go in reverse to calculate the mincost for each day
+            j = i # for each day, we pick the min from below loop
             
-            dp[i] = float("inf")
-            j = i
-
             for cost, duration in zip(costs, [1, 7, 30]):
-                print(j)
-                while j < len(days) and days[j] < duration + days[i]: # cover the duration for different tickets
+                while j < len(days) and days[j] < days[i] + duration: 
                     j += 1
-
-                dp[i] = min(dp[i], cost + dfs(j))
-                print("end")
-            
-            return dp[i]
+                
+                dp[i] = min(dp[i], cost + dp[j]) # dp[j] is already calculated
         
-        return dfs(0)
- 
+        return dp[0]
