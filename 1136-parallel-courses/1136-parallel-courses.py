c21e51b63f27cf85e6923ff1e1b9b21cb16b6b9f
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-06-22 13:32:45 -0400

    Time: 7 ms (94.78%), Space: 19.6 MB (86.6%) - LeetHub

--- 1136-parallel-courses/1136-parallel-courses.py
+++ 1136-parallel-courses/1136-parallel-courses.py
@@ -2,27 +2,29 @@ class Solution:
     def minimumSemesters(self, n: int, relations: List[List[int]]) -> int:
         adj = defaultdict(list)
         indegree = [0] * (n + 1)
+        queue = deque()
+        semesters, finished = 0, 0
 
         for prevC, nextC in relations:
             adj[prevC].append(nextC)
             indegree[nextC] += 1
         
-        queue = deque()
-        for i in range(1, n + 1):
+        for i in range(1, n+1):
             if indegree[i] == 0:
                 queue.append(i)
         
-        taken = 0
-        semesters = 0
-
         while queue:
             for _ in range(len(queue)):
                 course = queue.popleft()
-                taken += 1
+                finished += 1
                 for nei in adj[course]:
                     indegree[nei] -= 1
+                    
                     if indegree[nei] == 0:
                         queue.append(nei)
+            
             semesters += 1
         
-        return semesters if taken == n else -1
+        return semesters if finished == n else -1
+
+        
