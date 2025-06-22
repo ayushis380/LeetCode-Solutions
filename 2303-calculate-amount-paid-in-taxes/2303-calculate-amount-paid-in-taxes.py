2804ebeb46599af33024730afa74714b7b4dd0ba
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-06-22 12:59:26 -0400

    Time: 0 ms (100%), Space: 18 MB (36.71%) - LeetHub

--- 2303-calculate-amount-paid-in-taxes/2303-calculate-amount-paid-in-taxes.py
+++ 2303-calculate-amount-paid-in-taxes/2303-calculate-amount-paid-in-taxes.py
@@ -1,18 +1,10 @@
 class Solution:
     def calculateTax(self, brackets: List[List[int]], income: int) -> float:
-        taxes = 0
-        incomeLeft = income 
-
-        for i in range(len(brackets)):
-            if i == 0:
-                toBeTaxed = min(brackets[i][0], incomeLeft)
-            else:
-                toBeTaxed = min(brackets[i][0] - brackets[i-1][0], incomeLeft)
-            
-            taxes += toBeTaxed * (brackets[i][1] / 100)
-            incomeLeft -= toBeTaxed
-        
-        return taxes
-
+        ans = prev = 0 
+        for hi, pct in brackets: 
+            hi = min(hi, income)
+            ans += (hi - prev)*pct/100
+            prev = hi 
+        return ans 
 
 
