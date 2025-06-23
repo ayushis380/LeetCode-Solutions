9aaac34551d378ccc56078c15299c60e11f7a35a
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-06-23 00:08:18 -0400

    Time: 79 ms (53.55%), Space: 18.2 MB (77.23%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,38 +1,28 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(t) > len(s):
-            return ""
-        
-        t_cnt = Counter(t)
-        s_cnt = Counter()
-        need, have = len(t_cnt), 0
+        s_ct = Counter()
+        t_ct = Counter(t)
         start = 0
+        have, need = 0, len(t_ct)
         minlen = float("inf")
-        left, right = -1, -1
+        l, r = -1, -1
 
-        for end, ch in enumerate(s):
-            s_cnt[ch] += 1
-
-            if ch in t_cnt and t_cnt[ch] == s_cnt[ch]:
+        for i, ch in enumerate(s):
+            s_ct[ch] += 1
+            if ch in t_ct and s_ct[ch] == t_ct[ch]:
                 have += 1
             
             while have == need:
-                length = end - start + 1
-                
+                length = i - start + 1
                 if minlen > length:
                     minlen = length
-                    left, right = start, end + 1
+                    l, r = start, i
                 
                 ch_start = s[start]
-                s_cnt[ch_start] -= 1 # look for smaller window and see if count condition is still satisfied
-
-                if ch_start in t_cnt and s_cnt[ch_start] < t_cnt[ch_start]:
-                    have -= 1 # condition need to have >= : means freq of a character in s can be greater than freq of that same character in t but not less 
+                s_ct[ch_start] -= 1
+                if ch_start in t_ct and s_ct[ch_start] < t_ct[ch_start]:
+                    have -= 1
                 
-                start += 1 # keep decreasing the window - until have != need
+                start += 1
         
-        return s[left: right]
-
-            
-
-
+        return s[l: r+1]
