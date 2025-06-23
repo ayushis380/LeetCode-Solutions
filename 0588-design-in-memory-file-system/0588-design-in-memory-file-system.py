8e8e3ad8d51d0c86bc803cf23412511f97da620a
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-06-23 11:27:41 -0400

    Time: 6 ms (17.4%), Space: 18.6 MB (30.36%) - LeetHub

--- 0588-design-in-memory-file-system/0588-design-in-memory-file-system.py
+++ 0588-design-in-memory-file-system/0588-design-in-memory-file-system.py
@@ -1,35 +1,32 @@
 class FileSystem:
 
     def __init__(self):
-        self.paths = defaultdict(list) # path - > list of filenames and dir under that path 
-        self.files = defaultdict(str) # filename -> content (in str)
-        
+        self.paths = defaultdict(list)
+        self.files = defaultdict(str)
 
     def ls(self, path: str) -> List[str]:
-        if path in self.files: # if path is a file path then return the file name
-            return [path.split("/")[-1]] # eg a/b/c/d.js
+        if path in self.files:
+            return [path.split("/")[-1]]
         else:
             return self.paths[path]
 
     def mkdir(self, path: str) -> None:
-        directories = path.split("/") 
-
-        # there can be middle directories that aren't made, so check that
-        # /a/b = splitiing this gives ["","a","b"], so start range from 1
+        directories = path.split("/")
 
         for i in range(1, len(directories)):
             cur = "/".join(directories[:i]) or "/"
-            
-            # if cur is not defined or sub directory is not in current path
+
             if cur not in self.paths or directories[i] not in self.paths[cur]:
-                bisect.insort(self.paths[cur], directories[i]) # inserts in lexicographic order, or use binary search to insert at correct position
+                bisect.insort(self.paths[cur], directories[i])
         
+        print("paths: ", self.paths)
+        print("files: ", self.files)
 
     def addContentToFile(self, filePath: str, content: str) -> None:
         if filePath not in self.files:
             self.mkdir(filePath)
         
-        self.files[filePath] += content # append the content
+        self.files[filePath] += content
 
     def readContentFromFile(self, filePath: str) -> str:
         return self.files[filePath]
