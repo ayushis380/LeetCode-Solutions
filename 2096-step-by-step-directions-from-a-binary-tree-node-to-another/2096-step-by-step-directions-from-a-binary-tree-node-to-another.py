04ce489c10dd9c3a5bb0609955732036df8b3adf
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-06-23 10:45:05 -0400

    Time: 93 ms (49.37%), Space: 54.9 MB (83.14%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -6,45 +6,37 @@
 #         self.right = right
 class Solution:
     def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
-        parentMap = {} # node -> parent
-        queue = deque()
-        visited = set()
-
-        def dfs(node):
+        # build the path for start and dest values - it will be same till the LCA of these nodes
+        # after LCA. start and dest will be in different dir
+        # so L of start if converted to U and R of dest will give us the full path
+        def dfs(node, path, target):
             if not node:
-                return
-            
-            if node.val == startValue:
-                queue.append((node, ""))
-                visited.add(node)
-            
-            if node.left:
-                parentMap[node.left] = node
-                dfs(node.left)
-            if node.right:
-                parentMap[node.right] = node
-                dfs(node.right)
+                return None # false
+            if node.val == target:
+                return path
+#      When res = None, it evaluates to False.
+# When res = path (a non-empty list), it evaluates to True.
+# So as long as res is not falsy, the DFS will return the path
+            path.append("L")
+            res = dfs(node.left, path, target)
+            if res: return res
+
+            path.pop()
+            path.append("R")
+            res = dfs(node.right, path, target)
+            if res: return res
+
+            path.pop() # if none of the values match then go in diffrent direction by popping all 
+            return None
         
-        dfs(root)
+        start = dfs(root, [], startValue)
+        dest = dfs(root, [], destValue)
+        i = 0
 
-        while queue:
-            for _ in range(len(queue)):
-                node, path = queue.popleft()
-                if node.val == destValue:
-                    return path
-                
-                if node in parentMap:
-                    parent = parentMap[node]
-                    if parent not in visited:
-                        queue.append((parent, path + "U"))
-                        visited.add(parent)
-                
-                if node.left and node.left not in visited:
-                    queue.append((node.left, path + "L"))
-                    visited.add(node.left)
-                
-                if node.right and node.right not in visited:
-                    queue.append((node.right, path + "R"))
-                    visited.add(node.right)
+        while i < min(len(start), len(dest)):
+            if start[i] != dest[i]: # first non matching value
+                break
+            i += 1
+        res = ["U"] * len(start[i:]) + dest[i:]
+        return "".join(res)
 
-        return ""
