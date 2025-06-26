95d67386ba55b507a4f0911e36cf43a6d6416745
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-06-26 12:03:21 -0400

    Time: 389 ms (41.23%), Space: 19.8 MB (53.96%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,25 +1,24 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        adjlist = defaultdict(list)
-        heap = [(0, k)] # it can have multiple entries for a node, as we can have multiple paths to reach a node but min heap ensures we take the minimum cost path
+        adj = defaultdict(list)
+        visited = set()
+        heap = [(0, k)]
         delaytime = 0
-        visited = set() # helps check is node is already visited that means the node was already reached with a lower cost
 
         for u, v, w in times:
-            adjlist[u].append((v,w))
-        
+            adj[u].append((v, w))
+
         while heap:
             time, node = heapq.heappop(heap)
-            if node in visited: # already visited means seen with a lower cost
+            if node in visited:
                 continue
             
-            delaytime = max(delaytime, time) # max seen so far to reach all the nodes
+            delaytime = max(delaytime, time)
             visited.add(node)
-            
-            # going from source to the other nodes
-            for nei, weight in adjlist[node]:
+
+            for nei, wt in adj[node]:
                 if nei not in visited:
-                    heapq.heappush(heap, (time + weight, nei))
+                    heapq.heappush(heap, (time + wt, nei))
         
-        # not necessarily that we reach all the nodes
-        return delaytime if len(visited) == n else -1
+        return delaytime if len(visited) == n else -1 
+
