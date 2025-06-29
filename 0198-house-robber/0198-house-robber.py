d5d000c0d910032ae6406a1bc79d83765f6f8aa1
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-06-29 14:42:00 -0400

    Time: 0 ms (100%), Space: 17.8 MB (54.55%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,13 +1,13 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-#  if we are solving for an array size of 6, we need an array of size 7 to store the results of the subproblems for 0 to 6 houses. The final answer will be stored in dp[6]
-        dp = [-1] * (len(nums) + 1)
-        dp[0] = 0
-        dp[1] = nums[0] # dp[i] will represent the optimal solution for the first i houses
+        prev2, prev1 = 0, 0
+        arr = []
 
-        for i in range(2, len(nums) + 1):
-            take = dp[i-2] + nums[i-1]
-            skip = dp[i-1]
-            dp[i] = max(take, skip)
+        for i in range(len(nums)):
+            tmp = prev2 + nums[i]
+            prev2 = prev1
+            prev1 = max(tmp, prev1)
+            arr.append(prev1)
         
-        return dp[len(nums)]
+        print(arr)
+        return prev1
