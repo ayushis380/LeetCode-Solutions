3a6967ddd06e83dc417ae9ff9cebb3a931ef6c46
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-07-01 19:40:04 -0400

    Time: 205 ms (7.76%), Space: 22.8 MB (18.8%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,21 +1,25 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-    # we are traversing the grid from left to right, and from top to bottom, for each land cell we are currently at, we only need to check whether the LEFT and UP cells are land cells with a slight modification on previous approach
-        
-        rows = len(grid)
-        cols = len(grid[0])
-        
-        result = 0
+        rows, cols = len(grid), len(grid[0])
+        visited = set()
+
+        def dfs(r, c):
+            if r >= rows or r < 0 or c >= cols or c < 0 or grid[r][c] == 0:
+                return 1
+            
+            if (r, c) in visited:
+                return 0
+            
+            visited.add((r,c))
+            perimeter = 0
+
+            for dr, dc in [[-1,0], [1,0], [0,1], [0,-1]]:
+                nr, nc = r + dr, c + dc
+                perimeter += dfs(nr, nc)
+
+            return perimeter
         
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 1:
-                    result += 4
-                    
-                    if r > 0 and grid[r-1][c] == 1: # up
-                        result -= 2 # -2 as two boundaries are common here, in old and new cell
-                        
-                    if c > 0 and grid[r][c-1] == 1: # left
-                        result -= 2
-        
-        return result
+                    return dfs(r, c)
