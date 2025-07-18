f195140eb4133b12b134ab5478f5d68751409e90
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-07-17 23:40:16 -0400

    Time: 139 ms (68.9%), Space: 48.5 MB (40.35%) - LeetHub

--- 2050-parallel-courses-iii/2050-parallel-courses-iii.py
+++ 2050-parallel-courses-iii/2050-parallel-courses-iii.py
@@ -1,27 +1,24 @@
 class Solution:
     def minimumTime(self, n: int, relations: List[List[int]], time: List[int]) -> int:
+        dp = {}
         adj = defaultdict(list)
-        queue = deque()
-        indegree = [0] * (n +1)
-        dp = [0] * (n+1)
 
         for u, v in relations:
             adj[u].append(v)
-            indegree[v] += 1
+
+        def dfs(src):
+            if src in dp:
+                return dp[src]
+            
+            res = time[src-1] # makes sure we take course's own month as some wont have neighbors
+            for nei in adj[src]:
+                res = max(res, time[src-1] + dfs(nei))
+            
+            dp[src] = res
+            return res
         
         for i in range(1, n+1):
-            if indegree[i] == 0:
-                queue.append(i)
-                dp[i] = time[i-1]
+            dfs(i)
 
-        
-        while queue:
-            crs = queue.popleft()
-            for nei in adj[crs]:
-                dp[nei] = max(dp[nei], dp[crs] + time[nei-1])
-                indegree[nei] -= 1
-                
-                if indegree[nei] == 0:
-                    queue.append(nei)
-        print(dp)
-        return max(dp)
+        # print(dp)
+        return max(dp.values())
