2fe297df52dd91ca56e394df9196739156eb1bbe
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-07-17 21:21:03 -0400

    Time: 0 ms (100%), Space: 18.1 MB (14.35%) - LeetHub

--- 2303-calculate-amount-paid-in-taxes/2303-calculate-amount-paid-in-taxes.py
+++ 2303-calculate-amount-paid-in-taxes/2303-calculate-amount-paid-in-taxes.py
@@ -1,11 +1,11 @@
 class Solution:
     def calculateTax(self, brackets: List[List[int]], income: int) -> float:
         taxes = 0
-        total = 0
+        prev = 0
 
         for upper, per in brackets:
-            amount = min(upper, income) - total
-            taxes += (amount * per) /100
-            total += amount
+            upper = min(upper, income)
+            taxes += (upper - prev) * (per/100)
+            prev = upper
         
         return taxes
