956728a346f6741e95878738ae08aef920e43363
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-07-18 19:26:15 -0400

    Time: 21 ms (76.84%), Space: 18.2 MB (78.64%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -1,31 +1,32 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        sign = 1 # 1 for positive, -1 for negative
+        stack = [] # to store old result when we get ( ) case
+        num = 0
+        sign = 1
         res = 0
-        operand = 0
-        stack = []
 
         for ch in s:
             if ch.isdigit():
-                operand = operand * 10 + int(ch)
-            elif ch == '+':
-                res += sign * operand # existing sign and operand
+                num = num * 10 + int(ch)
+            elif ch == "+":
+                res += sign * num
                 sign = 1
-                operand = 0
-            elif ch == '-':
-                res += sign * operand
+                num = 0
+            elif ch == "-":
+                res += sign * num
                 sign = -1
-                operand = 0
-            elif ch == '(': 
-                stack.append(res) # appending seen result 
+                num = 0
+            elif ch == "(":
+                stack.append(res)
                 stack.append(sign)
-
+                
                 sign = 1
-                res = 0 # start new result from ( bracket
+                res = 0 # new result with ( )
             elif ch == ")":
-                res += sign * operand # result in between ( ) these twp
-                res *= stack.pop() # pop the old sign
-                res += stack.pop() # pop the old result
-                operand = 0
+                res += sign * num
+                res *= stack.pop()
+                res += stack.pop()
+
+                num = 0
         
-        return res + sign * operand # make sure last operand is evaluated because we evaluate left when we go to right - but for last operand the loop exists before
+        return res + sign * num
