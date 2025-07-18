0569370c1c3b5e4f920b31ce9b00be064efb667e
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-07-17 20:57:58 -0400

    Time: 71 ms (76.96%), Space: 54.9 MB (83.7%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -6,37 +6,35 @@
 #         self.right = right
 class Solution:
     def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
-        # build the path for start and dest values - it will be same till the LCA of these nodes
-        # after LCA. start and dest will be in different dir
-        # so L of start if converted to U and R of dest will give us the full path
         def dfs(node, path, target):
             if not node:
-                return None # false
+                return None
+            
             if node.val == target:
                 return path
-#      When res = None, it evaluates to False.
-# When res = path (a non-empty list), it evaluates to True.
-# So as long as res is not falsy, the DFS will return the path
+            
             path.append("L")
             res = dfs(node.left, path, target)
             if res: return res
-
+            
             path.pop()
             path.append("R")
             res = dfs(node.right, path, target)
             if res: return res
-
-            path.pop() # if none of the values match then go in diffrent direction by popping all 
+            
+            path.pop()
             return None
         
-        start = dfs(root, [], startValue)
-        dest = dfs(root, [], destValue)
-        i = 0
+        sarr = dfs(root, [], startValue)
+        darr = dfs(root, [], destValue)
 
-        while i < min(len(start), len(dest)):
-            if start[i] != dest[i]: # first non matching value
+        i = 0
+        while i < min(len(sarr), len(darr)):
+            if sarr[i] != darr[i]:
                 break
             i += 1
-        res = ["U"] * len(start[i:]) + dest[i:]
+
+        res = ["U"] * len(sarr[i:]) + darr[i:]
         return "".join(res)
 
+                
