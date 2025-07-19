73c31f7222ccffb09e62800607b591b1203bb759
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-07-18 23:21:25 -0400

    Time: 391 ms (7.46%), Space: 70.3 MB (12.3%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -1,34 +1,25 @@
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-    # think of the subproblem : include or not include the job and when reach the end, return 0
-    # sort it on basis of start time, so its easier to check which job has started and look for no overlaps
-        intervals = sorted(zip(startTime, endTime, profit)) # dont lose the order
-        cache = {} # memoiz
+    # for each job, there are two options, either to schedule it or not
+# schedule the job at index i that ends at endTime[i], then all the jobs which have a startTime before endTime[i] can be discarded.
+        jobs = sorted(zip(startTime, endTime, profit))
+        dp = {}
 
         def dfs(i):
-            if i == len(intervals):
+            if i == len(jobs):
                 return 0
             
-            if i in cache:
-                return cache[i]
+            if i in dp:
+                return dp[i]
             
-            # dont include
-            res = dfs(i + 1) # go to next index
-
-            # include
-            # j = i + 1 # looking for next valid index j with index i
-
-            # below while loop causes N^2 complexity as its runnign under dfs
-            # while j < len(intervals):
-            #     if intervals[i][1] <= intervals[j][0]: # end time of i <= start time of j - so valid j value found
-            #         break
-            #     j += 1 # if not found then increase j
-
-            # looking for a start time that is greater than equal to current index end time - run a binary search as values are sorted
-            # placed at start time position - looking for intervals[i][1]
-            j = bisect.bisect(intervals, (intervals[i][1], -1, -1)) # intervals is a tuple so need same format
-            
-            cache[i] = res = max(res, intervals[i][2] + dfs(j)) # include i profit and call dfs on j (as j is the next valid index)
-            return res
+            res = dfs(i + 1) # not take i 
+            # jobs[i][1] is end time of i => current job
+            # in the list of start times for all jobs
+            j = bisect.bisect(jobs, (jobs[i][1], -1, -1))
+            print("for i ", i, " j is : ", j)
+            dp[i] = max(res, jobs[i][2] + dfs(j))
+            return dp[i]
         
+        dfs(0)
+        print(list(dp.items()))
         return dfs(0)
