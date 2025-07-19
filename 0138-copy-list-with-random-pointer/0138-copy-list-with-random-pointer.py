b6c6d898e504d6f40f8c5d6848c8a6795e4e1c06
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-07-19 12:58:23 -0400

    Time: 37 ms (81.7%), Space: 18.5 MB (84.57%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -1,41 +1,42 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
 class Solution:
-    def copyRandomList(self, head: "Optional[Node]") -> "Optional[Node]":
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if not head:
-            return head
-
-        # Creating a new weaved list of original and copied nodes.
+            return None
+        
         ptr = head
+        # create interweaved list 
         while ptr:
-
-            # Cloned node
-            new_node = Node(ptr.val, None, None)
-
-            # Inserting the cloned node just next to the original node.
-            # If A->B->C is the original linked list,
-            # Linked list after weaving cloned nodes would be A->A'->B->B'->C->C'
-            new_node.next = ptr.next
-            ptr.next = new_node
-            ptr = new_node.next
-
+            cpy = Node(ptr.val)
+            
+            cpy.next = ptr.next
+            ptr.next = cpy
+            ptr = ptr.next.next
+        
+        # assign the random pointers
         ptr = head
-
-        # Now link the random pointers of the new nodes created.
-        # Iterate the newly created list and use the original nodes random pointers,
-        # to assign references to random pointers for cloned nodes.
         while ptr:
             ptr.next.random = ptr.random.next if ptr.random else None
             ptr = ptr.next.next
+        
+        # unweave the list
+        old_head = head
+        new_head = head.next
+        result = head.next
+        
+        while old_head:
+            old_head.next = old_head.next.next
+            new_head.next = new_head.next.next if new_head.next else None
 
-        # Unweave the linked list to get back the original linked list and the cloned list.
-        # i.e. A->A'->B->B'->C->C' would be broken to A->B->C and A'->B'->C'
-        ptr_old_list = head  # A->B->C
-        ptr_new_list = head.next  # A'->B'->C'
-        head_new = head.next
-        while ptr_old_list:
-            ptr_old_list.next = ptr_old_list.next.next
-            ptr_new_list.next = (
-                ptr_new_list.next.next if ptr_new_list.next else None
-            )
-            ptr_old_list = ptr_old_list.next
-            ptr_new_list = ptr_new_list.next
-        return head_new
+            old_head = old_head.next
+            new_head = new_head.next
+        
+        return result
