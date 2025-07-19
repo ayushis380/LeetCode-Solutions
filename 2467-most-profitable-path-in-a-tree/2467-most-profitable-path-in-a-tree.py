16812585ce30627db0d0113832b78a8a8e8188d9
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-07-19 18:37:06 -0400

    Time: 204 ms (77.08%), Space: 67.5 MB (40.41%) - LeetHub

--- 2467-most-profitable-path-in-a-tree/2467-most-profitable-path-in-a-tree.py
+++ 2467-most-profitable-path-in-a-tree/2467-most-profitable-path-in-a-tree.py
@@ -1,57 +1,54 @@
 class Solution:
     def mostProfitablePath(self, edges: List[List[int]], bob: int, amount: List[int]) -> int:
-        # n nodes and n-1 edges : undirected acyclic graph
         adj = defaultdict(list)
+        bobtime = defaultdict(int)
+
         for u, v in edges:
             adj[u].append(v)
             adj[v].append(u)
         
-        # Bob simulation, DFS, he is moving towards root - only one path choice
-# DFS is a natural choice because it fully explores each path before backtracking, allowing us to efficiently find the path Bob follows to the root.
-        bobTime = defaultdict(int) # node on root path -> timestamp he visited
-
-        def dfs(src, prev, ts):
-            if src == 0:
-                bobTime[src] = ts
+        def dfs(node, prev, ts):
+            if node == 0:
+                bobtime[node] = ts
                 return True
             
-            for nei in adj[src]:
+            for nei in adj[node]:
                 if nei == prev:
-                    continue # avoid cycle, shouldnt go where we came from
-                if dfs(nei, src, ts+1): # only becomes True when it gets True from the root 0
-                    bobTime[src] = ts
+                    continue
+                if dfs(nei, node, ts + 1):
+                    bobtime[node] = ts
                     return True
             
             return False
-            
+        
         dfs(bob, -1, 0)
 
-        # Alice simulation, will always have amount of root node as bob != 0
-        # discovers a lot of paths and picks maximum 
-        # BFS systematically explores all nodes level by level, making it ideal for finding optimal paths
-        q = deque([(0, 0, -1, amount[0])]) # node, time, parent, total profit
-        res = float("-inf")
+        queue = deque([(0, 0, amount[0], -1)]) # ts = 0, node = 0, amt = amount[0], prev = -1
+        income = float("-inf")
 
-        while q:
-            node, time, parent, profit = q.popleft()
+        while queue:
+            ts, node, profit, prev = queue.popleft()
 
             for nei in adj[node]:
-                if nei == parent: # avoid cycle
+                if nei == prev:
                     continue
-
-                nei_profit = amount[nei]
-                nei_time = time + 1 # nei time, all neis of node will be visited at time + 1 as only one path is considered 
-
-                if nei in bobTime:
-                    if nei_time > bobTime[nei]: # if bob has reached the node before alice
-                        nei_profit = 0
-                    elif nei_time == bobTime[nei]: # if eqaul then divide it
-                        nei_profit = nei_profit // 2 # if negative then also fine as amount is always even
                 
-                q.append((nei, nei_time, node, profit + nei_profit)) # keep building profit from the neighbors
+                nei_profit = amount[nei]
+                nei_time = ts + 1
 
-            # this len check is added under nei for loop as if moved outside then it would take root's amount if it also had a len of 1 - so take only its neigbors and their neigbors
-                if len(adj[nei]) == 1: # this is the leaf node - so one of the possible paths
-                    res = max(res, profit + nei_profit)
+                if nei in bobtime:
+                    if nei_time > bobtime[nei]:
+                        nei_profit = 0
+                    elif nei_time == bobtime[nei]:
+                        nei_profit = nei_profit//2
+                
+                queue.append((nei_time, nei, profit + nei_profit, node))
+
+                if len(adj[nei]) == 1:
+                    income = max(income, profit + nei_profit)
         
-        return res
+        return income
+                
+                
+
+        
