19598e53a2251b75a3922a6509ba399f0989d282
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-07-19 00:14:25 -0400

    Time: 7 ms (90.23%), Space: 20.3 MB (36.84%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,30 +5,23 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        interval = 1
-        n = len(lists)
-
-        while interval < n:
-            for i in range(0, n - interval, interval * 2):
-                print("i is ", i)
-                lists[i] = self.merge(lists[i], lists[i + interval])
-            
-            interval *= 2
+        if not lists:
+            return None
         
-        return lists[0] if n > 0 else None
-    
-    def merge(self, l1, l2):
+        heap = []
         dummy = ListNode(-1)
         cur = dummy
 
-        while l1 and l2:
-            if l1.val < l2.val:
-                cur.next = l1
-                l1 = l1.next
-            else:
-                cur.next = l2
-                l2 = l2.next
-            cur = cur.next
+        for i, lst in enumerate(lists):
+            if lst:
+                heapq.heappush(heap, (lst.val, i, lst))
         
-        cur.next = l1 if l1 is not None else l2
+        while heap:
+            val, i, lst = heapq.heappop(heap)
+            cur.next = ListNode(val)
+            cur = cur.next
+
+            if lst.next:
+                heapq.heappush(heap, (lst.next.val, i, lst.next))
+        
         return dummy.next
