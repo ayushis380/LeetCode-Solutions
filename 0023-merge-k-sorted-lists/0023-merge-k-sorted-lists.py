200ffc1ec3b1e1f54fef0750b3e3e121002a8bff
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-07-19 00:09:17 -0400

    Time: 12 ms (48.46%), Space: 20 MB (89.81%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,39 +5,30 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        length = len(lists)
         interval = 1
+        n = len(lists)
 
-# interval * 2 is used to control the gap between pairs being merged, and it doubles each iteration to reduce the number of lists logarithmically
-# i jumps by interval * 2 because each merge operation combines two intervals of length interval
-
-        while interval < length:
-            for i in range(0, length - interval, interval * 2):
+        while interval < n:
+            for i in range(0, n - interval, interval * 2):
                 print("i is ", i)
-                lists[i] = self.mergeTwoLists(lists[i], lists[i+interval])
+                lists[i] = self.merge(lists[i], lists[i + interval])
             
-            interval *= 2 # less steps in next iteration 
+            interval *= 2
         
-        return lists[0] if length > 0 else None
+        return lists[0] if n > 0 else None
     
-    def mergeTwoLists(self, l1, l2):
-        head = ListNode(0)
-        cur = head 
-# not creating new nodes in this approach - its just placing the pointers in right way
+    def merge(self, l1, l2):
+        dummy = ListNode(-1)
+        cur = dummy
 
         while l1 and l2:
-            if l1.val > l2.val:
-                cur.next = l2
-                l2 = l2.next
-            else:
+            if l1.val < l2.val:
                 cur.next = l1
                 l1 = l1.next
-            
+            else:
+                cur.next = l2
+                l2 = l2.next
             cur = cur.next
         
-        if l1:
-            cur.next = l1
-        if l2:
-            cur.next = l2
-        
-        return head.next
+        cur.next = l1 if l1 is not None else l2
+        return dummy.next
