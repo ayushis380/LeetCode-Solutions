85b8b6b4c04943cf052bb67b9a78e4bfc08ee46b
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-07-20 23:46:31 -0400

    Time: 103 ms (89.14%), Space: 57.3 MB (97.65%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,42 +1,32 @@
 class RandomizedSet:
 
     def __init__(self):
-    # HashSet can't be used - we cant index a hash set
-    # as the values are stored in random order, wont be able to fetch through an index
-
-        self.numMap = {} # val -> index (where its stored in numList)
-        self.numList = [] 
-        
+        self.hashlist = []
+        self.hashmap = {}
 
     def insert(self, val: int) -> bool:
-        res = val not in self.numMap
-        if res:
-            self.numMap[val] = len(self.numMap)
-            self.numList.append(val)
+        if val not in self.hashmap:
+            self.hashlist.append(val)
+            self.hashmap[val] = len(self.hashlist) - 1
+            return True
         
-        return res
-        
+        return False
 
     def remove(self, val: int) -> bool:
-        res = val in self.numMap
-        # removing the element from a list is difficult as it needs adjusting costing O(n) 
-        # so remove the last element from list as it O(1)
-        # removed last element will be placed at val's index - dont forget to update the map as it stores the index of last val
+        if val in self.hashmap:
+            ind = self.hashmap[val]
+            lastVal = self.hashlist[-1]
 
-        if res:
-            idx = self.numMap[val] # index of element to be removed
-            lastVal = self.numList[-1] # last val
-            self.numList[idx] = lastVal # place last val at idx (val's index)
-            self.numMap[lastVal] = idx # update index in map
-            self.numList.pop() # remove last element
-            del self.numMap[val]
+            self.hashlist[ind] = lastVal
+            self.hashmap[lastVal] = ind
+            self.hashlist.pop()
+            del self.hashmap[val]
+            return True
         
-        return res
-
+        return False
 
     def getRandom(self) -> int:
-        return random.choice(self.numList) # remaining elements have same probability 
-        
+        return random.choice(self.hashlist)
 
 
 # Your RandomizedSet object will be instantiated and called as such:
