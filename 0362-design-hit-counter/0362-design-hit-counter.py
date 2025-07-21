dbca74bda13f597d761b737f409e06ae113c2574
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-07-20 23:30:06 -0400

    Time: 0 ms (100%), Space: 18.1 MB (19.27%) - LeetHub

--- 0362-design-hit-counter/0362-design-hit-counter.py
+++ 0362-design-hit-counter/0362-design-hit-counter.py
@@ -1,28 +1,31 @@
-from collections import deque
-
 class HitCounter:
-# that all the timestamps that we will encounter are going to be in increasing order
-# we have to ignore the timestamps that occurred previously (having a difference of 300 or more with the latest timestamp)
+
     def __init__(self):
-        self.hits = deque()  # stores (timestamp, count)
-        self.total = 0       # running total of hits in the last 300 seconds
+        self.queue = deque()
+        self.total = 0 
 
     def hit(self, timestamp: int) -> None:
-        # If the last timestamp is the same, just update count
-        if self.hits and self.hits[-1][0] == timestamp:
-            self.hits[-1] = (timestamp, self.hits[-1][1] + 1)
+        if self.queue and self.queue[-1][0] == timestamp:
+            self.queue[-1][1] += 1
         else:
-            self.hits.append((timestamp, 1))
-
-        self.total += 1
+            self.queue.append([timestamp, 1])
+        
+        self.total += 1 # each hit, add to total
         self._cleanup(timestamp)
 
     def getHits(self, timestamp: int) -> int:
         self._cleanup(timestamp)
         return self.total
-
-    def _cleanup(self, timestamp: int) -> None:
-        # Remove all entries older than 300 seconds
-        while self.hits and self.hits[0][0] <= timestamp - 300:
-            old_timestamp, count = self.hits.popleft()
+    
+    def _cleanup(self, timestamp):
+        while self.queue and self.queue[0][0] <= timestamp - 300:
+            ts, count = self.queue.popleft()
             self.total -= count
+        
+        return self.total
+
+
+# Your HitCounter object will be instantiated and called as such:
+# obj = HitCounter()
+# obj.hit(timestamp)
+# param_2 = obj.getHits(timestamp)
