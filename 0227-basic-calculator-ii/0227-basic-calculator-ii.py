eb617e70f4d625239cc316204f905274ad5e197e
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-07-20 22:23:30 -0400

    Time: 79 ms (41.75%), Space: 21.4 MB (61.76%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,30 +1,28 @@
 class Solution:
     def calculate(self, s: str) -> int:
+        prev = "+"
         stack = []
-        prev_op = '+' # to start as addition and subtraction have low priority 
         num = 0
 
-# when at last index - we havent done the last operation seen
-# so go till +1 to do that operation
-# we do the operation once we have enough digits to perform that operation
-
-        for i in range(len(s) + 1): # go +1 as to process the last operation 
-            ch = s[i] if i < len(s) else '\0' # '\0' is taken as it doesnt match digits and ops
+        for i in range(0, len(s) + 1):
+            ch = s[i] if i < len(s) else '\0'
 
             if ch.isdigit():
                 num = num * 10 + int(ch)
             
-            if not ch.isdigit() and ch != ' ' or i == len(s):
-                if prev_op == "+":
+            if not ch.isdigit() and ch != " " or ch == "\0":
+                if prev == "+":
                     stack.append(num)
-                elif prev_op == "-":
+                elif prev == "-":
                     stack.append(-num)
-                elif prev_op == "*":
-                    stack.append(stack.pop() * num)
-                elif prev_op == "/":
-                    stack.append(int(stack.pop() / num))
+                elif prev == "*":
+                    v1 = stack.pop()
+                    stack.append(v1 * num)
+                elif prev == "/":
+                    v1 = stack.pop()
+                    stack.append( int(v1/ num))
                 
-                prev_op = ch # in next iteration - do this operation
+                prev = ch
                 num = 0
         
         return sum(stack)
