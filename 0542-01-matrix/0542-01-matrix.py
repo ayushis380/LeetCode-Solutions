72e26bfcc17d1f8d0e0122ebae186ec3f7c448b3
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-07-22 18:36:09 -0400

    Time: 199 ms (27.82%), Space: 21 MB (34.52%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,11 +1,8 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-# If we start BFS from 1, we can only find the shortest distance for that 1. If we start BFS from 0, we could find the shortest distance for many 1 at a time. starting from 1 or 0 both will take x steps, but starting from 0 is efficient
-
-        matrix = mat
-        rows, cols = len(mat) , len(mat[0])
-        visited = set()
+        rows, cols = len(mat), len(mat[0])
         queue = deque()
+        visited = set()
 
         for r in range(rows):
             for c in range(cols):
@@ -14,13 +11,12 @@ class Solution:
                     visited.add((r, c))
         
         while queue:
-            r, c, level = queue.popleft()
-
-            for dr, dc in [[-1,0], [1,0], [0,-1], [0,1]]:
+            r, c, dist = queue.popleft()
+            for dr, dc in [[-1,0], [1, 0], [0,1], [0, -1]]:
                 nr, nc = r + dr, c + dc
                 if 0 <= nr < rows and 0 <= nc < cols and (nr, nc) not in visited:
-                    matrix[nr][nc] = level + 1
-                    queue.append((nr, nc, level +1))
+                    queue.append((nr, nc, dist + 1))
                     visited.add((nr, nc))
+                    mat[nr][nc] = dist + 1
         
-        return matrix
+        return mat
