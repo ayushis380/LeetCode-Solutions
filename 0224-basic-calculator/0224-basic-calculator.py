137e18f7835d23cc8824461c5536f625613af3b3
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-07-22 21:22:49 -0400

    Time: 18 ms (95.39%), Space: 18.2 MB (78.29%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -1,9 +1,9 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        stack = [] # to store old result when we get ( ) case
+        stack = []
+        res = 0
         num = 0
         sign = 1
-        res = 0
 
         for ch in s:
             if ch.isdigit():
@@ -16,13 +16,13 @@ class Solution:
                 res += sign * num
                 sign = -1
                 num = 0
-            elif ch == "(":
+            elif ch == '(':
                 stack.append(res)
                 stack.append(sign)
-                
+
+                res = 0
                 sign = 1
-                res = 0 # new result with ( )
-            elif ch == ")":
+            elif ch == ')':
                 res += sign * num
                 res *= stack.pop()
                 res += stack.pop()
