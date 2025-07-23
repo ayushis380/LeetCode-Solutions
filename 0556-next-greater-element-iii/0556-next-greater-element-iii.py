185a62b55814694e4c6cceebf7cdb96c5ff30b41
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-07-22 23:25:01 -0400

    Time: 0 ms (100%), Space: 17.6 MB (98.28%) - LeetHub

--- 0556-next-greater-element-iii/0556-next-greater-element-iii.py
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.py
@@ -1,21 +1,22 @@
 class Solution:
     def nextGreaterElement(self, n: int) -> int:
-        digits = list(str(n))
-        length = len(digits)
+        num = list(str(n))
+        length = len(num)
 
-        i = length - 2
-        while i >=0 and digits[i] >= digits[i+1]: # find the smallest value from end as replacing it will give next greater value
+        i = length -2 # first smallest from end
+        while i >= 0 and num[i] >= num[i+1]:
             i -= 1
         
-        if i == -1:
-            return -1 # number is the highest value that can be formed from digits, so return -1
+        if i < 0:
+            return -1
         
-        j = length - 1 # start from end
-        while digits[j] <= digits[i]: # till we find a value just greater than smallest value found
+        j = length - 1 # just greater than value at num[i]
+        while num[j] <= num[i]:
             j -= 1
-        digits[i], digits[j] = digits[j], digits[i] # swap
+        
+        num[j], num[i] = num[i], num[j]
+        num[i+1:] = reversed(num[i+1:])
+        result = int(''.join(num))
 
-        digits[i+1:] = sorted(digits[i+1:]) # next perm
-        result = int("".join(digits)) # digits is a list
+        return result if result <= 2 ** 31 - 1 else -1
 
-        return result if result <= 2**31 - 1 else -1 
