e6e74bf0f7234791de30a8ef5d35d729668b8328
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-07-22 23:51:24 -0400

    Time: 0 ms (100%), Space: 17.8 MB (56.53%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -10,12 +10,11 @@ class Solution:
         while i >= 0 and nums[i] >= nums[i+1]:
             i -= 1
         
-        if i >=0:
-            j = n -1
-            while nums[j] <= nums[i]:
+        if i >= 0:
+            j = n - 1
+            while j >= 0 and nums[j] <= nums[i]:
                 j -= 1
             nums[i], nums[j] = nums[j], nums[i]
         
-        nums[i+1:] = sorted(nums[i+1:])
-
+        nums[i+1:] = reversed(nums[i+1:])
         
