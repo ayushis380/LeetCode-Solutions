097ab355003f1b0117813bc630411e636ddb5352
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-08-11 23:34:23 -0400

    Time: 3 ms (14.36%), Space: 18.1 MB (5.21%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.py
+++ 1929-concatenation-of-array/1929-concatenation-of-array.py
@@ -1,14 +1,10 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
         length = len(nums)
-        ans = [0] * 2 * length # creates a space for array - after that only the index assignment can be done. it's important to ensure the index exists within the list's range before modifying it
-        # ans = [] # append and insert safely add the new element
-        
+        result = [0] * 2 * length
+
         for i in range(length):
-            ans[i] = nums[i]
-            ans[i+length] = nums[i]
-            
-            # ans.insert(i, nums[i]) # using this makes O(n ^2) as insert op has O(n) complexity
-            # ans.insert(i+length, nums[i])
+            result[i] = nums[i]
+            result[i + length] = nums[i]
         
-        return ans
+        return result
