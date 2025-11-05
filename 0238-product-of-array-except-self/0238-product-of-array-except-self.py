c37ecdf23662eccd23bfff3e43ea1298ccd2ea5b
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-05 14:35:35 -0500

    Time: 19 ms (87.14%), Space: 23.4 MB (55.38%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -6,9 +6,8 @@ class Solution:
             product[i] = product[i-1] * nums[i-1]
         
         post = 1
-
-        for i in range(len(nums) -1, -1, -1):
-            product[i] *= post
+        for i in range(len(nums) - 1, -1, -1):
+            product[i] = product[i] * post
             post *= nums[i]
         
         return product
