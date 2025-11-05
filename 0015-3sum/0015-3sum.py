c94fdecc58d0eb03fd347458e0a41cf7f6de7fb3
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-05 16:50:15 -0500

    Time: 595 ms (44.18%), Space: 20.7 MB (71.22%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -2,31 +2,26 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         self.result = []
+        length = len(nums)
 
-        for i in range(len(nums)):
+        for i in range(length):
             if i == 0 or nums[i-1] != nums[i]:
-                if nums[i] <= 0: # already sorted, so values ahead of index i will be bigger than 0 
-                    self.twoSum(nums, i)
+                self.twoSum(i, nums)
         
         return self.result
     
-    def twoSum(self, nums, i):
-        val = nums[i]
-        low = i + 1
-        high = len(nums) - 1
+    def twoSum(self, i, nums):
+        low, high = i+1, len(nums) - 1
 
         while low < high:
-            total = val + nums[low] + nums[high]
-            
+            total = nums[i] + nums[low] + nums[high]
             if total == 0:
-                self.result.append([val, nums[low], nums[high]])
+                self.result.append([nums[low], nums[i], nums[high]])
                 low += 1
                 high -= 1
                 while low < high and nums[low-1] == nums[low]:
                     low += 1
-            elif total > 0:
-                high -= 1
-            else:
+            elif total < 0:
                 low += 1
-        
-
+            else:
+                high -= 1
