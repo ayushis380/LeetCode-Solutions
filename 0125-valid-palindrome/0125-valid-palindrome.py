40145dd11fcb022336440c0321c93f143cced042
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-05 16:18:07 -0500

    Time: 7 ms (82.39%), Space: 18.2 MB (55.57%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,20 +1,17 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        low = 0
-        high = len(s) - 1
+        low, high = 0, len(s) - 1
 
-        while low < high:
-            while low < high and not s[low].isalnum(): # low <= high condition shouldnt come as it will make low and high out of bounds
+        while low <= high:
+            while low < high and not s[low].isalnum():
                 low += 1
-            
             while low < high and not s[high].isalnum():
                 high -= 1
             
-            print(low)
             if s[low].lower() != s[high].lower():
                 return False
-        
+
             low += 1
-            high -=1
-        
+            high -= 1
+            
         return True
