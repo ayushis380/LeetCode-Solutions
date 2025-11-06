e87fabba0eebfd8bdafedf6857f96a577d57d1bd
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-05 20:07:50 -0500

    Time: 71 ms (24.66%), Space: 17.9 MB (62.05%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -3,23 +3,20 @@ class Solution:
         if len(s1) > len(s2):
             return False
         
-        s1_ct = Counter(s1)
-        s2_ct = Counter()
+        # window of s1 len if present in s2
+        count_s1 = Counter(s1)
+        count_s2 = Counter()
+        length = len(s1)
         start = 0
 
         for end, ch in enumerate(s2):
-            ch_start = s2[start]
-            s2_ct[ch] += 1
+            count_s2[ch] += 1
 
-            if end >= len(s1):
-                s2_ct[ch_start] -= 1
+            if end - start + 1 > length:
+                count_s2[s2[start]] -= 1
                 start += 1
-
-                if s2_ct[ch_start] == 0:
-                    del s2_ct[ch_start]
             
-            if s1_ct == s2_ct:
+            if count_s1 == count_s2:
                 return True
-            
+        
         return False
-
