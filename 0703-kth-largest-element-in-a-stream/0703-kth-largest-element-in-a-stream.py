198cc110776b4bfa9c35afc901e1153fe748f9de
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-07 16:35:35 -0500

    Time: 12 ms (78.95%), Space: 23.8 MB (29.81%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.py
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.py
@@ -3,18 +3,21 @@ class KthLargest:
     def __init__(self, k: int, nums: List[int]):
         self.heap = []
         self.k = k
-# keeping track of just the k largest elements allows us to efficiently maintain the k-th largest element
-# If an incoming element val is smaller than or equal to the existing k-th largest element: The k largest elements remain unchanged, and we can return the current k-th largest element.
-# If val is larger than the current k-th largest element: It replaces the current k-th largest element. After adding val, the new k-th largest element is the next largest element.
-        for score in nums:
-            self.add(score)
+
+        for n in nums:
+            heapq.heappush(self.heap, n)
+
+            if len(self.heap) > k:
+                heapq.heappop(self.heap)
 
     def add(self, val: int) -> int:
         heapq.heappush(self.heap, val)
+
         if len(self.heap) > self.k:
-                heapq.heappop(self.heap)
+            heapq.heappop(self.heap)
         
         return self.heap[0]
+        
 
 
 # Your KthLargest object will be instantiated and called as such:
