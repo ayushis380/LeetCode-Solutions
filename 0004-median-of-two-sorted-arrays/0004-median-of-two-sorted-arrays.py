75de21f53731fec30cd0450dd905106373e7bdbe
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-06 19:48:52 -0500

    Time: 0 ms (100%), Space: 18 MB (58.7%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,7 +1,7 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         A, B = nums1, nums2
-        if len(A) > len(B):
+        if len(B) < len(A):
             A, B = B, A
         
         total = len(A) + len(B)
@@ -9,20 +9,22 @@ class Solution:
         low, high = 0, len(A) - 1
 
         while True:
-            i = low + (high - low)//2
-            j = half - i - 2
+            i = (low + high)//2
+            j = half - i -2
+            # print("i is - ", i, " j is - ", j)
 
-            A_left = A[i] if i >= 0 else float("-inf")
-            A_right = A[i+1] if (i + 1) < len(A) else float("inf")
+            Aleft = A[i] if i >=0 else float("-inf")
+            Aright = A[i+1] if (i + 1) < len(A) else float("inf")
 
-            B_left = B[j] if j >= 0 else float("-inf")
-            B_right = B[j+1] if (j + 1) < len(B) else float("inf")
+            Bleft = B[j] if j >= 0 else float("-inf")
+            Bright = B[j+1] if (j+ 1) < len(B) else float("inf")
 
-            if A_left <= B_right and B_left <= A_right:
-                if total%2:
-                    return min(A_right, B_right)
-                return (max(A_left, B_left) + min(A_right, B_right)) /2
-            elif A_left > B_right:
+            if Aleft <= Bright and Bleft <= Aright:
+                if total % 2: # odd len
+                    return min(Aright, Bright)
+                return (max(Aleft, Bleft) + min(Aright, Bright))/ 2
+            elif Aleft > Bright:
                 high = i - 1
             else:
                 low = i + 1
+            
