0dfd27bc35586544a179b753209109188e361493
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-07 17:15:49 -0500

    Time: 100 ms (15.02%), Space: 22.8 MB (40.57%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,22 +1,17 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-#  Python, on the other hand, uses TimSort, which is a hybrid of MergeSort and InsertionSort and requires O(N) extra space. 
-        heap = [] # max heap
-        closest = []
-        
+        heap = []
+        result = []
+
         for x, y in points:
-# simplify the process of comparing two points by using the squared Euclidean distance instead of the precise Euclidean distance, as both will yield the same result
-# significantly reduce the overall computational time for each comparison made
-            dist = -1 * (x ** 2 + y ** 2)
-            heapq.heappush(heap, (dist, x, y))
+            dist = sqrt(x ** 2 + y ** 2)
+            heapq.heappush(heap, (-dist, x, y))
 
             if len(heap) > k:
                 heapq.heappop(heap)
         
         while heap:
-            dist, x, y = heapq.heappop(heap)
-            closest.append([x, y])
+            d, x, y = heapq.heappop(heap)
+            result.append([x, y])
         
-        return closest
-
-        
+        return result
