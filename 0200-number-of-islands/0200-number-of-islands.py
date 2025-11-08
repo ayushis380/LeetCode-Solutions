d45fb72c6df4e3ee6c1b14c64f8e831265d8e668
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-08 18:32:22 -0500

    Time: 260 ms (39.28%), Space: 20.4 MB (42.88%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,34 +1,22 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-#at any point during BFS, the queue never holds more than O(min(M, N)) elements at once, because: The BFS proceeds layer by layer (level-order), and Each cell adds only a few neighbors (up to 4)
-
-# So even if the entire island is one big line, the maximum simultaneous neighbors in the queue will never exceed the length of the shortest side of the grid.
-
         rows, cols = len(grid), len(grid[0])
-        islands = 0
+        result = 0
 
+        def dfs(r, c):
+            if r >= rows or r < 0 or c >= cols or c < 0 or grid[r][c] == "0":
+                return 
+            
+            grid[r][c] = "0"
+
+            for dr, dc in [[-1,0], [1,0], [0,1], [0,-1]]:
+                nr, nc = r + dr, c + dc
+                dfs(nr, nc)
+        
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == "1":
-                    islands += 1
-                    grid[r][c] = 0
-                    neighbors = []
-                    neighbors.append((r, c))
-
-                    while neighbors:
-                        nr, nc = neighbors.pop(0)
-                        
-                        if nr -1 >=0 and grid[nr-1][nc] == "1":
-                            neighbors.append((nr -1, nc))
-                            grid[nr - 1][nc] = "0" # marking visited
-                        if nr +1 < rows and grid[nr + 1][nc] == "1":
-                            neighbors.append((nr + 1, nc))
-                            grid[nr + 1][nc] = "0"
-                        if nc -1 >=0 and grid[nr][nc -1] == "1":
-                            neighbors.append((nr, nc- 1))
-                            grid[nr][nc - 1] = "0"
-                        if nc +1 < cols and grid[nr][nc + 1] == "1":
-                            neighbors.append((nr, nc +1))
-                            grid[nr][nc + 1] = "0"
+                    dfs(r, c)
+                    result += 1
         
-        return islands
+        return result
