c8f3e9b88081aa50965b84546bd1e8ba3d115b8c
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-08 17:11:47 -0500

    Time: 0 ms (100%), Space: 17.9 MB (22.69%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,9 +1,6 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if not digits:
-            return []
-            
-        phone = {
+        pad = {
             "2": "abc",
             "3": "def",
             "4": "ghi",
@@ -13,22 +10,22 @@ class Solution:
             "8": "tuv",
             "9": "wxyz",
         }
-        
-        output = []
 
-        def dfs(i, path):
+        path = []
+        result = []
+
+        def dfs(i):
             if i >= len(digits):
-                output.append("".join(path.copy()))
+                result.append("".join(path))
                 return
-
-            dgt = digits[i]
-            val = phone[dgt]
-
-            for ch in val:
-                path.append(ch)
-                dfs(i+1, path)
-                path.pop()
             
+            dgt = digits[i]
+            letters = pad[dgt]
+            
+            for ch in letters:
+                path.append(ch)
+                dfs(i + 1)
+                path.pop()
         
-        dfs(0, [])
-        return output
+        dfs(0)
+        return result
