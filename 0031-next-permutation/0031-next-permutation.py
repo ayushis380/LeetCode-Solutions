cc49fa8587cccbc51784b18dddaecd85946a6371
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-08 15:34:00 -0500

    Time: 0 ms (100%), Space: 17.6 MB (85.98%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,20 +1,18 @@
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-    # 1. find the lowest value L from end of nums - as we need next greater value so this need to swapped with something big
-    # 2. swap L with next greater value than it from end - once placed the right values to it can be sorted to find next perm
-    # 3. sort from L's index +1 till end - 
+        length = len(nums)
+        i = length - 2
 
-        n = len(nums)
-        i = n - 2
-
-        while i >= 0 and nums[i] >= nums[i+1]:
+        while i >= 0 and nums[i] >= nums[i+1]: # find the smallest number from end
             i -= 1
         
-        if i >= 0:
-            j = n - 1
-            while j >= 0 and nums[j] <= nums[i]:
-                j -= 1
-            nums[i], nums[j] = nums[j], nums[i]
+        j = length - 1
+        # from end find just bigger num than nums[i] 
+        while j >= 0 and nums[j] <= nums[i]:
+            j -= 1
         
-        nums[i+1:] = reversed(nums[i+1:])
+        nums[i], nums[j] = nums[j], nums[i]
+        nums[i+1:] = sorted(nums[i+1:])
+
+
         
