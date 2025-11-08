0b3543a2e1b38533f4dd995e35a52c456a338429
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-07 19:44:06 -0500

    Time: 127 ms (96.94%), Space: 40.2 MB (60.4%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,31 +1,32 @@
 class MedianFinder:
 
     def __init__(self):
-        # small is max heap, large is min heap
-        self.small, self.large = [], []
+        self.left, self.right = [], []
 
     def addNum(self, num: int) -> None:
-        if self.large and num > self.large[0]:
-            heapq.heappush(self.large, num)
+        if self.right and num > self.right[0]:
+            heapq.heappush(self.right, num)
         else:
-            heapq.heappush(self.small, -1 * num)
+            heapq.heappush(self.left, -1 * num)
         
-        if len(self.large) > len(self.small) + 1:
-            val = -1 * heapq.heappop(self.large)
-            heapq.heappush(self.small, val)
-        elif len(self.small) > len(self.large) + 1:
-            val = -1 * heapq.heappop(self.small)
-            heapq.heappush(self.large, val)
+        if len(self.left) > len(self.right) + 1:
+            val = heapq.heappop(self.left) * -1
+            heapq.heappush(self.right, val)
+        elif len(self.right) > len(self.left) + 1:
+            val = heapq.heappop(self.right) * -1
+            heapq.heappush(self.left, val)
+        
 
     def findMedian(self) -> float:
-        if len(self.large) != len(self.small):
-            if len(self.large) > len(self.small):
-                return self.large[0]
-            else:
-                return -1 * self.small[0]
-        else:
-            return (self.large[0] + (-1 * self.small[0]))/2
+        total = len(self.left) + len(self.right)
 
+        if total % 2: # odd
+            if len(self.left) > len(self.right): # consider the bigger one, as its top element will be the median
+                return -1 * self.left[0]
+            else:
+                return self.right[0]
+        else:
+            return (-1 * self.left[0] + self.right[0])/2
         
 
 
