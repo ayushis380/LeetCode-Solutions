8942296a63fae053d3021be5c7f7e9e899d84cdf
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-07 20:21:42 -0500

    Time: 3 ms (10.56%), Space: 18.1 MB (21.03%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,17 +1,19 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         result = []
+        path = [] # keeping outside and not as a param of dfs(), otherwise func will have to store more variables
 
-        def dfs(i, path):
+        def dfs(i):
             if i >= len(nums):
                 result.append(path.copy())
                 return
             
             path.append(nums[i])
-            dfs(i+1, path)
+            dfs(i + 1)
 
             path.pop()
-            dfs(i+1, path)
+            dfs(i + 1)
+
         
-        dfs(0, [])
+        dfs(0)
         return result
