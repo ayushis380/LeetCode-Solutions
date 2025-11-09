14707d2ea05737ab6a69b8582739f63fa0393a05
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-09 15:52:40 -0500

    Time: 3 ms (47.25%), Space: 19.6 MB (24.31%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -1,37 +1,23 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        parent = list(range(n))
-        rank = [0] * n
-        components = n
-
-        def find(n):
-            if parent[n] != n:
-                n = find(parent[n])
-            return parent[n]
-
-        def union(u, v):
-            nonlocal components
-
-            root_u = find(u)
-            root_v = find(v)
-
-            if root_u == root_v:
-                return True
-            
-            if rank[root_u] < rank[root_v]:
-                parent[root_u] = root_v
-                rank[root_v] += 1
-            else:
-                parent[root_v] = root_u
-                rank[root_u] += 1
-            
-            components -= 1
-
-            return False 
+        # using dfs
+        adjlist = defaultdict(list)
+        visited = set()
 
         for u, v in edges:
-            if union(u, v):
-                return False
+            adjlist[u].append(v)
+            adjlist[v].append(u)
         
-        # print(components)
-        return True if components == 1 else False
+        def dfs(node, prev): # ignore the neighbor from where we came
+            if node in visited:
+                return False
+            
+            visited.add(node)
+            for nei in adjlist[node]:
+                if nei != prev:
+                    if not dfs(nei, node):
+                        return False
+
+            return True
+        
+        return dfs(0, -1) and len(visited) == n
