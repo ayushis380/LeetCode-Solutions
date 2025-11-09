19363f1fe4107a7518f79ce5a0d13dfd80350c6a
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-08 21:42:08 -0500

    Time: 3 ms (81.53%), Space: 17.9 MB (25.56%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,30 +1,35 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
-        time = 0
         queue = deque()
-        freshCount = 0
+        fresh = 0
+        time = 0
 
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 2:
-                    queue.append((r,c))
+                    queue.append((r, c))
                 if grid[r][c] == 1:
-                    freshCount += 1
+                    fresh += 1
         
-        if len(queue):
-            time -= 1 # for adding all rotten oranges
-
+        if not fresh:
+            return 0
+        
         while queue:
             curlen = len(queue)
-            time += 1
-            for i in range(curlen):
+            time += 1 # all rotten oranges in queue will convert its fresh o to rotten for this timestamp.
+
+            for _ in range(curlen):
                 r, c = queue.popleft()
-                for dr, dc in [[-1,0], [1,0], [0,1],[0,-1]]:
-                    nr, nc = r +dr, c + dc
+                for dr, dc in [[-1,0], [1,0], [0,-1],[0,1]]:
+                    nr, nc = r + dr, c + dc
                     if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1:
                         grid[nr][nc] = 2
+                        fresh -= 1
                         queue.append((nr, nc))
-                        freshCount -= 1
+            
+                    if fresh == 0:
+                        return time
 
-        return time if freshCount == 0 else -1
+        return -1
+
