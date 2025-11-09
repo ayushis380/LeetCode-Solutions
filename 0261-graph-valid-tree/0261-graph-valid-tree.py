40591df1fcebf567de69b6fb33cc30a661873440
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-09 02:28:39 -0500

    Time: 7 ms (13.25%), Space: 19.1 MB (51.2%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -1,37 +1,37 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
         parent = list(range(n))
-        rank = [1] * n
+        rank = [0] * n
         components = n
 
-        def find(node):
-            if parent[node] != node:
-                parent[node] = find(parent[node])
-            return parent[node]
-        
+        def find(n):
+            if parent[n] != n:
+                n = find(parent[n])
+            return parent[n]
+
         def union(u, v):
             nonlocal components
+
             root_u = find(u)
             root_v = find(v)
 
             if root_u == root_v:
                 return True
             
-            if rank[root_u] != rank[root_v]:
-                if rank[root_u] > rank[root_v]:
-                    parent[root_v] = root_u
-                elif rank[root_u] < rank[root_v]:
-                    parent[root_u] = root_v
-            else:
+            if rank[root_u] < rank[root_v]:
                 parent[root_u] = root_v
                 rank[root_v] += 1
+            else:
+                parent[root_v] = root_u
+                rank[root_u] += 1
             
             components -= 1
 
-            return False
-        
+            return False 
+
         for u, v in edges:
             if union(u, v):
                 return False
         
+        # print(components)
         return True if components == 1 else False
