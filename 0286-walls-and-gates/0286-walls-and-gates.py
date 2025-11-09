af91d1da7465993bff36bbc6264e22f63daae0e4
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-08 20:48:10 -0500

    Time: 42 ms (56.26%), Space: 21.1 MB (91.33%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -7,11 +7,17 @@ class Solution:
             for c in range(cols):
                 if rooms[r][c] == 0:
                     queue.append((r, c, 0))
-        
+                    
         while queue:
-            r, c, dist = queue.popleft()
-            for dr, dc in [[-1,0], [1,0], [0,1], [0,-1]]:
-                nr, nc = r + dr, c + dc
-                if 0 <= nr < rows and 0 <= nc < cols and rooms[nr][nc] == 2147483647:
-                    queue.append((nr, nc, dist + 1))
-                    rooms[nr][nc] = dist + 1
+            curlen = len(queue)
+            
+            for _ in range(curlen):
+                r, c, dist = queue.popleft()
+                for dr, dc in [[-1, 0], [1, 0], [0, -1], [0, 1]]:
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < rows and 0 <= nc < cols and rooms[nr][nc] == 2147483647:
+                        rooms[nr][nc] = dist + 1
+                        queue.append((nr, nc, dist + 1))
+    
+
+        
