c8eb7c1c68d551becfb5ffcc82e56fa7c890a5ce
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-09 02:46:43 -0500

    Time: 3 ms (90.62%), Space: 18.9 MB (88.68%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,30 +1,33 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        parent = list(range(n)) # each node pointing to itself
-        rank = [0] * n
-        components = n # at start all nodes are considered as not connected
+        parent = list(range(n))
+        size = [1] * n
+        components = n
 
-        def find(node):
-            if parent[node] != node:
-                parent[node] = find(parent[node]) # path compression
-            return parent[node]
+        def find(n):
+            if n != parent[n]:
+                parent[n] = find(parent[n])
+            return parent[n]
         
         def union(u, v):
             nonlocal components
             root_u = find(u)
             root_v = find(v)
 
-            if root_u != root_v: # if roots are not same then decrease the count
-                if rank[root_u] < rank[root_v]: # if rank = 2, rank = 1 then attaching them still gives rank = 2, so rank is not updated
-                    parent[root_u] = root_v
-                elif rank[root_u] > rank[root_v]:
-                    parent[root_v] = root_u
-                else: # when rank is same then attach and increase rank, shallow tree
-                    parent[root_u] = root_v
-                    rank[root_u] += 1
-                components -= 1
-
+            if root_u == root_v:
+                return 
+            
+            if size[root_u] < size[root_v]:
+                parent[root_u] = root_v
+                size[root_v] += size[root_u]
+            else:
+                parent[root_v] = root_u
+                size[root_u] += size[root_v]
+            
+            components -= 1
+        
         for u, v in edges:
             union(u, v)
         
         return components
+
