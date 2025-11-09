ad724cb06c333a3f93aa5cae58abd4b58e2796a9
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-08 22:41:04 -0500

    Time: 45 ms (21.75%), Space: 19.5 MB (21.99%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -3,29 +3,29 @@ class Solution:
         rows, cols = len(heights), len(heights[0])
         pac, atl = set(), set()
         result = []
-
-        def dfs(r, c, visit, prevHt):
-            if r >= rows or r < 0 or c >= cols or c < 0 or prevHt > heights[r][c] or (r, c) in visit:
+# each ocean has its set - we are trying to see if we can reach from ocean to the cell, cond reversed 
+# if we can move from boundaries to inward all the reachable neighbors can be reached from respective ocean     
+        def dfs(r, c, prevHt, visited):
+            if r >= rows or r < 0 or c >= cols or c < 0 or heights[r][c] < prevHt or (r, c) in visited:
                 return 
             
-            visit.add((r, c))
-
-            dfs(r +1, c, visit, heights[r][c])
-            dfs(r -1, c, visit, heights[r][c])
-            dfs(r, c + 1, visit, heights[r][c])
-            dfs(r, c - 1, visit, heights[r][c])
+            visited.add((r, c))
+            
+            for dr, dc in [[-1, 0], [1, 0], [0, 1], [0, -1]]:
+                nr, nc = r + dr, c + dc
+                dfs(nr, nc, heights[r][c], visited)
 
         for r in range(rows):
-            dfs(r, 0, pac, heights[r][0])
-            dfs(r, cols - 1, atl, heights[r][cols - 1])
+            dfs(r, 0, heights[r][0], pac)
+            dfs(r, cols - 1, heights[r][cols-1], atl)
         
         for c in range(cols):
-            dfs(0, c, pac, heights[0][c])
-            dfs(rows - 1, c, atl, heights[rows - 1][c])
+            dfs(0, c, heights[0][c], pac)
+            dfs(rows - 1, c, heights[rows -1][c], atl)
         
         for r in range(rows):
             for c in range(cols):
                 if (r, c) in pac and (r, c) in atl:
-                    result.append([r,c])
+                    result.append([r, c])
         
         return result
