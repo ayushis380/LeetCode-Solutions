285d7f08ee06e36a1db6854cc38a1102329a24e9
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-10 03:21:58 -0500

    Time: 3 ms (7.96%), Space: 17.7 MB (53.72%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,13 +1,10 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        prev2, prev1 = 0, 0
-        arr = []
-
-        for i in range(len(nums)):
-            tmp = prev2 + nums[i]
-            prev2 = prev1
-            prev1 = max(tmp, prev1)
-            arr.append(prev1)
+        prev1, prev2 = 0, 0
         
-        print(arr)
-        return prev1
+        for i in range(len(nums)):
+            tmp = prev2
+            prev2 = max(prev1 + nums[i], prev2)
+            prev1 = tmp
+        
+        return prev2
