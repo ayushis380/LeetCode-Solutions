64aa89492b4d99ee4995877ea06575f6125a1f33
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-10 16:48:21 -0500

    Time: 684 ms (89.09%), Space: 18.1 MB (68.83%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,15 +1,13 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        length = amount + 1 # we take to simplify using it in range
-        dp = [(amount + 1)] * (length)
+        n = len(coins)
+        dp = [(amount + 1)] * (amount + 1) # for every amt we find the number of coins required
         dp[0] = 0
 
-        for amt in range(1, length):
+        for i in range(1, amount + 1):
+            amt = i
             for cn in coins:
-                if cn == amt: # found the exact coin for the amt
-                    dp[amt] = 1
-                elif amt > cn:
-                    dp[amt] = min(dp[amt], 1 + dp[amt-cn]) # min is required as there are multiple ways to form an amount but we need to consider the minimum way
-        
-        return dp[length-1] if dp[length-1] != (amount + 1) else -1 # we couldnt achieve amount
-        
+                if amt >= cn:
+                    dp[i] = min(dp[i], 1 + dp[amt-cn])
+
+        return dp[amount] if dp[amount] != (amount + 1) else -1
