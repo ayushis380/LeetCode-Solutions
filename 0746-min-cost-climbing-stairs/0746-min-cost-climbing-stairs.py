dd775d416c81ed80994f60479ac1fb2c7be30f93
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-10 03:02:32 -0500

    Time: 7 ms (21.25%), Space: 19.3 MB (15.75%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -9,9 +9,8 @@ class Solution:
             if dp[i] != -1:
                 return dp[i]
             
-            res = min(dfs(i+1), dfs(i+2)) + cost[i]
-            dp[i] = res
-            return res
+            dp[i] = min(dfs(i+1), dfs(i+2)) + cost[i]
+            return dp[i]
         
         dfs(0)
         return min(dp[0], dp[1])
