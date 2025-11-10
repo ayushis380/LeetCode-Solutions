397a0565e43b62ecf8778c8626d78536a69a1721
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-09 19:40:59 -0500

    Time: 1356 ms (52.29%), Space: 155.5 MB (24.57%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,31 +1,29 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        adjlist = defaultdict(list)
         N = len(points)
-        adj = defaultdict(list) # i = [cost, neighbour]
-
-        # updating adjacency list
-        for i in range(N):
-            x1, y1 = points[i]
-            for j in range(i+1, N):
-                x2, y2 = points[j]
-                dist = abs(x1 - x2) + abs(y1 - y2)
-                adj[i].append([dist,j])
-                adj[j].append([dist,i]) # undirected graph
-        
-        # prims algo
-        totalcost = 0
+        heap = [(0, 0)]
+        cost = 0
         visited = set()
-        minh = [(0,0)]
 
-        while len(visited) < N: # check until all points are visited
-            cost, pt = heapq.heappop(minh) # minimum distance/cost will be popped
-            if pt in visited:
-                continue
-            totalcost += cost
-            visited.add(pt) # taken the cost of this point, so add to visited set
-            
-            for neicost, nei in adj[pt]: # now all enighbors of pt added to heap
-                if nei not in visited:
-                    heapq.heappush(minh, (neicost, nei))
+        for i in range(N):
+            for j in range(i + 1, N):
+                dist = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
+                adjlist[i].append((dist, j))
+                adjlist[j].append((dist, i))
         
-        return totalcost
+
+        while len(visited) < N:
+            dist, node = heapq.heappop(heap)
+            
+            if node in visited:
+                continue
+            
+            cost += dist
+            visited.add(node)
+
+            for d, nei in adjlist[node]:
+                if nei not in visited:
+                    heapq.heappush(heap, (d, nei))
+        
+        return cost
