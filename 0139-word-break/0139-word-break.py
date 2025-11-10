28cf480c8b6922a60913dfbca0a4b3f5dd73a532
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-10 17:25:04 -0500

    Time: 2 ms (73.28%), Space: 17.8 MB (64.77%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,19 +1,18 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        length = len(s)
-        dp = [False] * (length + 1)
-        dp[length] = True
+        n = len(s)
+        dp = [False] * (n + 1)
+        dp[n] = True # end of s
 
-        for i in range(length - 1, -1, -1):
+        for i in reversed(range(n)):
             for wrd in wordDict:
-                if i + len(wrd) <= length and s[i: i+len(wrd)] == wrd:
-                    print(wrd)
-                    dp[i] = dp[i+len(wrd)]
+                wlen = len(wrd)
+                
+                if dp[i]:
+                    continue
+                if i + wlen <= n and s[i: i + wlen] == wrd:
+                    if dp[i + wlen] == True:
+                        dp[i] = True
         
-        # if once set to True then break as the other values in wordDict can set this dp value to False
-        # if found a True at ith index that means there is a possible way to form the string 
-        # break to move to last index of string s
-                if dp[i]: 
-                    break
-        print(dp)
         return dp[0]
+                
