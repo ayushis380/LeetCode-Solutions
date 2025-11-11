727c87876ae5100f83ebbd2f7d02b05855947d1d
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-11 01:17:26 -0500

    Time: 36 ms (32.97%), Space: 54.3 MB (99.54%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
@@ -1,13 +1,15 @@
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
-        check = set()
+        indexes = set() # to store ind of matching target
 
         for t in triplets:
             if t[0] > target[0] or t[1] > target[1] or t[2] > target[2]:
-                continue
+                continue # remove bad triplets which are greater than target
             
-            for ind in range(len(t)):
-                if t[ind] == target[ind]:
-                    check.add(ind)
+            # now remaining ones should be checked
+            for key, value in enumerate(t):
+                if value == target[key]:
+                    indexes.add(key)
         
-        return True if len(check) == 3 else False
+        return True if len(indexes) == 3 else False
+            
