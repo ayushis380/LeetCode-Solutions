34085317048238cc474fb07a7606cdbf3e29e717
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-11 03:06:49 -0500

    Time: 0 ms (100%), Space: 19.7 MB (60.08%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,17 +1,15 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        output = []
+        result = []
 
-        for i in range(len(intervals)):
-            # end of new Interval < start of exiting, so means nI is ending before
-            if newInterval[1] < intervals[i][0]:  
-                output.append(newInterval)
-                return output + intervals[i:]
-            elif newInterval[0] > intervals[i][1]: # start of nI is > end of existing, so nI will come afterwards
-                output.append(intervals[i])
+        for i, lst in enumerate(intervals):
+            if lst[1] < newInterval[0]: # end < nI_start
+                result.append(lst)
+            elif lst[0] > newInterval[1]: # start > nI_end
+                result.append(newInterval)
+                return result + intervals[i:]
             else:
-                newInterval = [min(intervals[i][0], newInterval[0]), 
-                max(newInterval[1], intervals[i][1])]
+                newInterval = [min(lst[0], newInterval[0]), max(lst[1], newInterval[1])]
         
-        output.append(newInterval)
-        return output
+        result.append(newInterval)
+        return result
