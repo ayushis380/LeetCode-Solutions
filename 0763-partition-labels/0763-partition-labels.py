ded9274ac96782d7c76670c5d8a936f3ec57ed25
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-11 01:53:31 -0500

    Time: 0 ms (100%), Space: 17.7 MB (83.56%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -1,18 +1,19 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        lastind = {} # to store last index of a char
-        res = []
-        for i, c in enumerate(s):
-            lastind[c] = i 
-        size, end = 0, 0
+        lastInd = {}
+        result = []
+        for i, ch in enumerate(s):
+            lastInd[ch] = i
         
-        for i, c in enumerate(s):
-            size += 1
-            end = max(end, lastind[c]) # only update if max for new char
-            if end == i: # for atmost condition
-                res.append(size)
+        size = 0
+        end = 0
+        for i in range(len(s)):
+            end = max(end, lastInd[s[i]])
+            if end > i:
+                size += 1
+            else:
+                result.append(size+1)
                 size = 0
-        
-        return res
+                end = 0
 
-        
+        return result
