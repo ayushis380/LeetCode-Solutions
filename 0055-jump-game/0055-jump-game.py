2ed95f457abb6cbcf7d5d7c18a2f9f4a3c073942
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-10 19:41:56 -0500

    Time: 16 ms (73.22%), Space: 18.8 MB (29.75%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,24 +1,10 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-    # DP 
-        dp = [-1] * len(nums) # a position can be -1 unknown, 0 bad, 1 good
-        dp[-1] = 1 # last index can reach itself so a good one
+        n = len(nums)
+        target = n - 1
 
-        def dfs(i):
-            if dp[i] != -1:
-                return dp[i] == 1
-            
-            # furthest jump from that index, compare with length as first param of min 
-            # can go out of bound
-            furthestJump = min(i + nums[i], len(nums) - 1)
-            # check for all next pos that can be reached from i
-            for nextposition in range(i+1, furthestJump+1):
-                if dfs(nextposition): # if able to reach the last index then True
-                    dp[i] = 1 # means we can reach the last index
-                    return True # we need to check once if possible to reach end
-            
-            dp[i] = 0 # bad pos as cant reach end
-            return False
+        for i in reversed(range(n)):
+            if i + nums[i] >= target:
+                target = i
         
-        return dfs(0)
-            
+        return True if target == 0 else False
