3a71ae45d0b50c6c879d1bb57165b6fe7210143a
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-13 14:50:11 -0500

    Time: 1327 ms (40.98%), Space: 65.5 MB (61.86%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -1,45 +1,41 @@
-class TrieNode:
+class Trie:
     def __init__(self):
         self.children = {}
-        self.end = False
+        self.endOfWord = False
 
 class WordDictionary:
 
     def __init__(self):
-        self.root = TrieNode()
+        self.root = Trie()
 
     def addWord(self, word: str) -> None:
         cur = self.root
-
         for ch in word:
             if ch not in cur.children:
-                cur.children[ch] = TrieNode()
+                cur.children[ch] = Trie()
             cur = cur.children[ch]
         
-        cur.end = True
-
+        cur.endOfWord = True
+        
     def search(self, word: str) -> bool:
-
+        
         def dfs(i, root):
             cur = root
-            
             for j in range(i, len(word)):
                 ch = word[j]
-
                 if ch == ".":
-                    for child in cur.children.values(): # need to explore all children of cur
-                        if dfs(j + 1, child): # if . then do dfs()
-                            return True # if any yield True then return 
+                    for v in cur.children.values():
+                        if dfs(j + 1, v):
+                            return True
                     return False
                 else:
                     if ch not in cur.children:
                         return False
-                    cur = cur.children[ch] # next children of cur, checked within same for loop, dfs() not required
+                    cur = cur.children[ch]
             
-            return cur.end # once the length is reached, end depicts if the word is present or not
-
-        return dfs(0, self.root)
+            return cur.endOfWord # this is imp, we dont put a check on the length of word(if i has reached end) as word might not be a part of Trie
         
+        return dfs(0, self.root)
 
 
 # Your WordDictionary object will be instantiated and called as such:
