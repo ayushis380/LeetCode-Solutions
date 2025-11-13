5994d2bb7ace79bb48c14b82150ce146ba831bc8
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-13 00:22:17 -0500

    Time: 387 ms (42.35%), Space: 19.9 MB (32.43%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,26 +1,22 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        heap = [(0, k)]
         adjlist = defaultdict(list)
-        heap = []
-        heapq.heappush(heap, (0, k)) # start from k
-        allReached = 0
         visited = set()
+        maxtime = 0
 
         for u, v, w in times:
             adjlist[u].append((v, w))
 
         while heap:
-            time, node = heapq.heappop(heap)
-    # a node can be reached through multiple paths - all possible time values are added to the heap, if its already visited then ignore as this time will impact the final result time
-            if node in visited: 
+            t, node = heapq.heappop(heap)
+            if node in visited:
                 continue
             
-            allReached = max(allReached, time)
+            maxtime = max(maxtime, t)
             visited.add(node)
-            
-            for nei, t in adjlist[node]:
+            for nei, time in adjlist[node]:
                 if nei not in visited:
-                    heapq.heappush(heap, (time + t, nei))
-        
-        return allReached if len(visited) == n else -1
+                    heapq.heappush(heap, (t + time, nei))
 
+        return maxtime if len(visited) == n else -1
