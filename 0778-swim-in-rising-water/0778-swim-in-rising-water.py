404d146ebcf5510573b87e7ccc336ec8bead12b1
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-12 23:50:50 -0500

    Time: 27 ms (62.82%), Space: 18.5 MB (37.73%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.py
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -1,19 +1,18 @@
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        # Dijkstras - bfs with min heap(priority queue)
-        # O(n^2 log n)
         n = len(grid)
-        heap = [(grid[0][0], 0, 0)] # to always pick the min t. answer is dictated by max of all these picked minimum ts
+        heap = [(grid[0][0], 0, 0)]
         visited = set([(0, 0)])
 
         while heap:
             t, r, c = heapq.heappop(heap)
-
-            if r == n -1 and c == n-1:
+            if r == n-1 and c == n-1:
                 return t
             
-            for dr, dc in [[-1,0], [1,0], [0,-1], [0,1]]:
+            for dr, dc in [[-1,0], [1,0], [0,1], [0,-1]]:
                 nr, nc = r + dr, c + dc
-                if 0 <= nr < n and 0<= nc < n and (nr, nc) not in visited:
+                if 0 <= nr < n and 0 <= nc < n and (nr, nc) not in visited:
                     visited.add((nr, nc))
                     heapq.heappush(heap, (max(t, grid[nr][nc]), nr, nc))
+        
+        return -1
