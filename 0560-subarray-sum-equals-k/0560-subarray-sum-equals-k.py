512a769d5e08e63b7b6cde9f91539e3dd10f3e9a
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-15 18:31:01 -0500

    Time: 36 ms (40.27%), Space: 20.2 MB (93.33%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,15 +1,16 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        presum = count = 0
-        sumarr = defaultdict(int)
-        sumarr[0] = 1
+        smap = defaultdict(int)
+        smap[0] = 1
+        presum = 0
+        count = 0
 
-        for i, val in enumerate(nums):
-            presum += val
-
-            if presum - k in sumarr:
-                count += sumarr[presum-k]
+        for n in nums:
+            presum += n
+            diff = presum - k
+            if diff in smap:
+                count += smap[diff]
             
-            sumarr[presum] += 1 # think if presum = x and we are looking for k then a presum whose value is (x-k) should be present 
+            smap[presum] += 1
         
         return count
