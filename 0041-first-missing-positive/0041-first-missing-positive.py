42f37077f1dac74814d58d04c3b384d29fd34843
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-15 18:16:23 -0500

    Time: 47 ms (49.57%), Space: 29 MB (43.43%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,12 +1,21 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        # O(nlogn) and O(1) 
-        # sorting and check for values from 1 to len(A)
-        nums.sort()
-        missing = 1 # first possible ans
+        n = len(nums)
+        # 0... n-1 indexes
+        # 1 ...n values - only these matter
+        i = 0
 
-        for n in nums: # checking for all values in sorted and finding missing
-            if missing == n:
-                missing += 1
-        
-        return missing
+        while i < n:
+            correct_pos = nums[i] - 1 # eg 1 then it should have been placed at index 0
+            
+            #  1 2 15 3 - bring 3 to index 2
+            if 1 <= nums[i] <= n and nums[correct_pos] != nums[i]: # bring values to correct pos
+                nums[i], nums[correct_pos] = nums[correct_pos], nums[i] # might need multiple swaps to place values at correct index- so i is not increased
+            else:
+                i += 1 # only increase when element is at correct index
+
+        for i in range(n):
+            if nums[i] != i + 1: # i in index so + 1
+                return i + 1
+
+        return n + 1                
