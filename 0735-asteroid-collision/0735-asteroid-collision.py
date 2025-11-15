491248c521298f14a8243bacbf804fed69b7464e
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-15 01:52:18 -0500

    Time: 5 ms (71.3%), Space: 18.7 MB (85.03%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -3,20 +3,18 @@ class Solution:
         stack = []
 
         for ast in asteroids:
-            while stack and stack[-1] > 0 and ast < 0:
+            while stack and stack[-1] > 0 and ast < 0 and ast != 0:
                 diff = stack[-1] + ast
 
-                if diff > 0:
-                    ast = 0
-                elif diff < 0:
+                if diff == 0:
                     stack.pop()
-    # not required below as we keep the original ast value when diff < 0, this ast is < 0 will be compared to pending values of the stack
-                    # ast = diff 
+                    ast = 0
+                elif diff < 0: # add ast
+                    stack.pop()
                 else:
-                    stack.pop()
-                    ast = 0
+                    ast = 0 # not add ast, as its -ve
             
-            if ast:
+            if ast != 0:
                 stack.append(ast)
         
         return stack
