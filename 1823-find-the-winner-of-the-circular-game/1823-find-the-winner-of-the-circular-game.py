b81697662f36fed51f350c5880188fb5e2854cb6
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-15 15:44:32 -0500

    Time: 0 ms (100%), Space: 18 MB (27.59%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,17 +1,15 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-    # time complexity is O(n⋅k) because each elimination cycle involves rotating the queue k−1 times, and this happens n−1 times.
-    # O(n) due to the queue storing all n friends initially.
-        q = deque()
+        return self.winnerHelper(n, k) + 1
 
-        for i in range(1, n+1): #fill the queue with friends labeled from 1 to n
-            q.append(i)
-        
-        while len(q) > 1:
-# we simulate the process by rotating the queue k-1 times. This action effectively moves the k-th friend to the front of the queue, ready for removal
-            for i in range(k -1): # under next k friends, the present friend is also included
-                q.append(q.popleft()) # add back
-            
-            q.popleft() # remove kth
-        
-        return q[0]
+    def winnerHelper(self, n: int, k: int) -> int:
+        if n == 1:
+            return 0
+        return (self.winnerHelper(n - 1, k) + k) % n
+
+# Iterative
+        # ans = 0
+        # for i in range(2, n + 1):
+        #     ans = (ans + k) % i
+        # # add 1 to convert back to 1 indexing
+        # return ans + 1
