2f96f0e70398c671e03c89055364f825f79530fa
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-15 15:51:08 -0500

    Time: 100 ms (21.87%), Space: 17.7 MB (71.22%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,15 +1,13 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        return self.winnerHelper(n, k) + 1
-
-    def winnerHelper(self, n: int, k: int) -> int:
-        if n == 1:
-            return 0
-        return (self.winnerHelper(n - 1, k) + k) % n
-
-# Iterative
-        # ans = 0
-        # for i in range(2, n + 1):
-        #     ans = (ans + k) % i
-        # # add 1 to convert back to 1 indexing
-        # return ans + 1
+        q = deque()
+        for i in range(1, n +1):
+            q.append(i)
+        
+        while len(q) > 1:
+            for i in range(k -1):
+                q.append(q.popleft())
+            
+            q.popleft()
+        
+        return q[0]
