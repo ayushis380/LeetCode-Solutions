4a32ea4aa22de4e4bddc62ec5a3f93c48951b88d
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-15 14:40:42 -0500

    Time: 145 ms (38.03%), Space: 17.7 MB (64.84%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,23 +1,17 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
+        self.count = 0
         n = len(s)
-        dp = [[False] * n for _ in range(n)]
-        result = 0 # all single chars are palindrome
 
         for i in range(n):
-            dp[i][i] = True
-            result += 1
+            self.palindrome(i, i, s)
+            self.palindrome(i, i + 1, s)
         
-        for i in range(n-1): # len = 2 check
-            if s[i] == s[i+1]:
-                dp[i][i+1] = True
-                result += 1
-            
-        for diff in range(2, n):
-            for i in range(n - diff):
-                j = i + diff #substring length difference diff, you’re checking substrings of length diff + 1
-                if s[i] == s[j] and dp[i+1][j-1]:
-                    dp[i][j] = True
-                    result += 1
+        return self.count
+    
+    def palindrome(self, l, r, s):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            self.count += 1
+            l -= 1
+            r += 1
         
-        return result
