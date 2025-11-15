4d022eaf263a0992cb203e12a56535195dc4b592
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-15 00:41:49 -0500

    Time: 87 ms (45.97%), Space: 18 MB (93.22%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,28 +1,30 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        s_ct = Counter()
-        t_ct = Counter(t)
+        if len(t) > len(s):
+            return ""
+        
+        s_map = Counter()
+        t_map = Counter(t)
+        need, have = len(t_map), 0
         start = 0
-        have, need = 0, len(t_ct)
+        left, right = 0, 0
         minlen = float("inf")
-        l, r = -1, -1
 
         for i, ch in enumerate(s):
-            s_ct[ch] += 1
-            if ch in t_ct and s_ct[ch] == t_ct[ch]:
+            s_map[ch] += 1
+            if ch in t_map and t_map[ch] == s_map[ch]:
                 have += 1
             
-            while have == need:
-                length = i - start + 1
-                if minlen > length:
-                    minlen = length
-                    l, r = start, i
+            while need == have:
+                if minlen > i - start + 1:
+                    minlen = i - start + 1
+                    left, right = start, i + 1
                 
-                ch_start = s[start]
-                s_ct[ch_start] -= 1
-                if ch_start in t_ct and s_ct[ch_start] < t_ct[ch_start]:
+                ch_st = s[start]
+                s_map[ch_st] -= 1
+                if ch_st in t_map and s_map[ch_st] < t_map[ch_st]:
                     have -= 1
                 
                 start += 1
         
-        return s[l: r+1]
+        return s[left: right]
