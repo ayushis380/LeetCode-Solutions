a4fc38d01c047683103c3d7e310bd32580c33066
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-16 03:14:56 -0500

    Time: 64 ms (43.8%), Space: 18.1 MB (35.44%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -2,19 +2,19 @@ class Solution:
     def validPalindrome(self, s: str) -> bool:
         low, high = 0, len(s) - 1
 
-        def check(l, h):
-            while l < h:
-                if s[l] != s[h]:
+        def dfs(low, high):
+            while low <= high:
+                if s[low] != s[high]:
                     return False
                 
-                l += 1
-                h -= 1
+                low += 1
+                high -= 1
             
             return True
-        
-        while low < high:
+
+        while low <= high:
             if s[low] != s[high]:
-                return check(low +1, high) or check(low, high -1)
+                return dfs(low +1, high) or dfs(low, high -1)
             
             low += 1
             high -= 1
