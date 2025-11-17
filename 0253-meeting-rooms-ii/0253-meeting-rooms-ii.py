21d025548011451c32b4b5b95fce37309b125edd
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-17 13:52:51 -0500

    Time: 8 ms (15.7%), Space: 21.3 MB (9.07%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,19 +1,18 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        intervals.sort(key = lambda x: x[0])
+        intervals.sort()
         time = []
+        rooms = 0
 
-        for start, end in intervals:
-            time.append([start, 1])
-            time.append([end, -1])
+        for s, e in intervals:
+            time.append([s, 1])
+            time.append([e, -1])
         
-        time.sort(key = lambda x: x[0])
-        maxrooms = 0
-        count = 0
+        time.sort()
 
+        count = 0
         for t, val in time:
             count += val
-            maxrooms = max(maxrooms, count)
+            rooms = max(rooms, count)
         
-        return maxrooms
-        
+        return rooms
