f2a25a071f47828d58ed77253e376d9cd1aa33ac
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-17 02:36:45 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.py
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.py
@@ -1,24 +1,24 @@
 class Solution:
-    def maxPalindromes(self, S: str, k: int) -> int:
-        N, intervals, last, ans = len(S), [], -inf, 0
-
-        # to get all palindromes of size k
-        for center in range(2 * N - 1):
-            left = center // 2
-            right = left + center % 2
-            while left >= 0 and right < N and S[left] == S[right]:
-                if right + 1 - left >= k: 
-                    intervals.append((left, right + 1)) # next palindrome should start from right + 1 for non overl
+    def maxPalindromes(self, s: str, k: int) -> int:
+        def palindrome(low, high):
+            while low >= 0 and high < len(s) and s[low] == s[high]:
+                if (high - low + 1) >= k:
+                    ranges.append([low, high + 1])
                     break
-                left -= 1
-                right += 1
+                low -= 1
+                high += 1
+
+        ranges = []
+        n = len(s)
+
+        for i in range(len(s)):
+            palindrome(i, i)
+            palindrome(i, i + 1)
+
+        ranges.sort(key = lambda x: x[1])
+        store = []
+        for s, e in ranges:
+            if not store or s > store[-1][1]:
+                store.append([s, e])
         
-        print(intervals)
-        
-        for x, y in intervals:
-            if x >= last: # if start of new palindrome >= end of last palindrome 
-                last = y # update where this palindrome ends
-                ans += 1
-            else:
-                if y < last: last = y # if overlap then take the smaller end value to make it non overlap, like done in intervals ques
-        return ans
+        return len(store)
