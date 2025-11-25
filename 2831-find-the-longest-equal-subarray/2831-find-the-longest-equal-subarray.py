e7bfced488cc0e663960c9af5d60a543e0803822
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-25 02:44:56 -0500

    Time: 396 ms (67.19%), Space: 39.3 MB (13.88%) - LeetHub

--- 2831-find-the-longest-equal-subarray/2831-find-the-longest-equal-subarray.py
+++ 2831-find-the-longest-equal-subarray/2831-find-the-longest-equal-subarray.py
@@ -1,21 +1,19 @@
 class Solution:
     def longestEqualSubarray(self, nums: List[int], k: int) -> int:
-        pos = defaultdict(list) # For every distinct number x, consider only the indices where x appears.
-        # To make them a continuous subarray, we may need to delete numbers in between them:
-        for i, n in enumerate(nums):
-            pos[n].append(i)
-        
+        ind = defaultdict(list)
         maxlen = 0
-        for n, ls in pos.items(): # # sliding window for each distinct number
+
+        for i, n in enumerate(nums):
+            ind[n].append(i)
+        
+        for n, ls in ind.items():
             l = 0
-            for r in range(len(ls)):
-                # window length of number - freq of numbers
-                # 1 3 2 3 1 3, 3 = [1, 3, 5], when l = 0, r = 2, it shows window from 1 to 5 and how many extra elements inbtwn
-                # l and r are like freq of number, r - l = 2 and there are two 3s
-                while ls[r] - ls[l] - (r - l) > k: # # while deletions needed > k, shrink window
-                    l += 1 
-                
-                maxlen = max(maxlen, (r - l + 1)) ## r - l + 1 = number of kept elements in window
+            length = len(ls)
+
+            for r in range(length):
+                while ls[r] - ls[l] - (r - l) > k:
+                    l += 1
+
+                maxlen = max(maxlen, r - l + 1)
 
         return maxlen 
-        
