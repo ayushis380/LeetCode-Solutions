3540568da79cfe41476176ea08cbf2845b02fc6d
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-27 15:57:22 -0500

    Time: 99 ms (64.44%), Space: 32.6 MB (30.14%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
@@ -1,14 +1,20 @@
 class Solution:
     def getKth(self, lo: int, hi: int, k: int) -> int:
         power = []
+        dp = defaultdict(int)
 
         def findPower(n):
+            if n in dp:
+                return dp[n]
+            
             if n == 1:
                 return 0
             if n % 2:
-                return 1 + findPower(n * 3 + 1)
+                dp[n] = 1 + findPower(n * 3 + 1)
             else:
-                return 1 + findPower(n//2)
+                dp[n] = 1 + findPower(n//2)
+            
+            return dp[n]
         
         for n in range(lo, hi + 1):
             value = findPower(n)
