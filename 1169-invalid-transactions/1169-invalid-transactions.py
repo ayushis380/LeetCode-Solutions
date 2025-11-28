cb9596e27380c148fdcc290b9342ee6dc240819e
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-28 15:01:20 -0500

    Time: 15 ms (66.96%), Space: 18.6 MB (46.63%) - LeetHub

--- 1169-invalid-transactions/1169-invalid-transactions.py
+++ 1169-invalid-transactions/1169-invalid-transactions.py
@@ -1,45 +1,36 @@
 class Solution:
     def invalidTransactions(self, transactions: List[str]) -> List[str]:
-        invalid = []
         timeMap = defaultdict(dict)
+        invalid = []
+        # {20 : { alice: {mtv} }, {50 : {alice: beijing}}
 
-        # {   
-        #     20: {'alice': {'mtv'}, 'bob': {'beijing'}}, 
-        #     50: {'bob': {'mtv'}}
-        #     }
         for trs in transactions:
             name, time, amount, city = trs.split(",")
             time = int(time)
 
             if name not in timeMap[time]:
-                timeMap[time][name] = {city}
+                timeMap[time][name] = {city} # new dict
             else:
                 timeMap[time][name].add(city)
-        
-        # for k, v in timeMap.items():
-        #     print("key, ", k, " value is ", v)
         
         for trs in transactions:
             name, time, amount, city = trs.split(",")
             time = int(time)
+            amount = int(amount)
 
-            if int(amount) > 1000:
+            if amount > 1000:
                 invalid.append(trs)
                 continue
             
-            for t in range(time - 60, time + 60): # look for all other transactions within range
+            for t in range(time - 60, time + 60):
                 if t not in timeMap:
                     continue
                 if name not in timeMap[t]:
                     continue
                 
-                trsNameTime = timeMap[t][name] # dict for t and name
-                # none of the checks above at 31 and 33 were true - so t and name have values in dict
-                # check on length as if a time-name key has more than 1 value in dict - they must be invalid
-                # eg 20: {'alice': {'mtv', 'beijing'}} or {'alice': {'mtv', 'mtv'}} - both are invalid, should be added to result
-
-                if city not in trsNameTime or len(trsNameTime) > 1: # check on length as 
+                tname = timeMap[t][name]
+                if city not in tname or len(tname) > 1:
                     invalid.append(trs)
                     break
-        
+            
         return invalid
