d16039b29cb37bc990fbd9479df4fbcc3c2fc149
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-27 19:11:38 -0500

    Time: 0 ms (100%), Space: 17.7 MB (67.58%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.py
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -1,23 +1,51 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        cur = self.root
+        
+        for ch in word:
+            if ch not in cur.children:
+                cur.children[ch] = TrieNode()
+            cur = cur.children[ch]
+        
+        cur.endOfWord = True
+
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        # O(n⋅2 ^n) = explore all paths, and forming string takes O(n)
-        # SC 2 ^n
-        wordSet = set(wordDict)
-        n = len(s)
-        result = []
-
-        def backtrack(start, path):
-            if start == n:
-                result.append(" ".join(path))
-                return
-            
-            for end in range(start + 1, n + 1): # n + 1 is important as we need end = n to exist
-                word = s[start:end] 
-                
-                if word in wordSet:
-                    path.append(word)
-                    backtrack(end, path) # end becomes new start
-                    path.pop()
+        # Trie implementation - check possible words s in reverse
+        # to compare them to the wordDict
+        trie = Trie()
+        dp = {}
+        for word in wordDict:
+            trie.insert(word)
         
-        backtrack(0, [])
-        return result
+        n = len(s)
+        for start in reversed(range(n)): # at every start ind from reverse
+            valid = []
+            cur = trie.root
+            for end in range(start, n): # start to n
+                ch = s[end]
+                if ch not in cur.children:
+                    break
+                
+                cur = cur.children[ch]
+                if cur.endOfWord: # present in trie
+                    word = s[start : end + 1]
+                    if end == n -1: # reached end, so full word needs to be appended
+                        valid.append(word)
+                    else:
+                        sentences_next = dp.get(end + 1, []) # form full string from the old values in dp
+                        for sntc in sentences_next:
+                            valid.append(word + " " + sntc) # word comes before the existing values as its in reverse
+
+            dp[start] = valid # store formed words/sentences
+        
+        return dp.get(0, []) # index 0 will have all valid sentences
+                
