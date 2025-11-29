e6b2d62ea3ad2304af068992496d543103d09143
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-28 23:06:28 -0500

    Time: 3 ms (80.31%), Space: 17.8 MB (64.44%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,26 +1,30 @@
-values = {
-    "I": 1,
-    "V": 5,
-    "X": 10,
-    "L": 50,
-    "C": 100,
-    "D": 500,
-    "M": 1000,
-}
-
-
 class Solution:
     def romanToInt(self, s: str) -> int:
+        num = {
+            "M" : 1000,
+            "CM": 900,
+            "D" : 500,
+            "CD" : 400,
+            "C" : 100,
+            "XC" : 90,
+            "L" : 50,
+            "XL" : 40,
+            "X": 10,
+            "IX" : 9,
+            "V" : 5,
+            "IV" : 4,
+            "I" : 1
+        }
+
+        n = len(s)
         total = 0
         i = 0
-        n = len(s)
-
         while i < n:
-            if i + 1 < n and values[s[i]] < values[s[i+1]]:
-                total += values[s[i+1]] - values[s[i]]
+            if i + 1 < n and num[s[i]] < num[s[i+1]]:
+                total += num[s[i: i+2]]
                 i += 2
             else:
-                total += values[s[i]]
+                total += num[s[i]]
                 i += 1
         
         return total
