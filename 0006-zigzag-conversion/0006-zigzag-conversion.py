a0bef5c8836b125df2a311f488f73ccaa26dbf1a
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-29 02:23:28 -0500

    Time: 15 ms (37.37%), Space: 18 MB (28.6%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,20 +1,18 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
+        # pattern between the distances between characters in each row
         if numRows == 1:
             return s
         
         result = ""
-
+        increment = 2 * (numRows - 1)
+        
         for r in range(numRows):
-             increment = 2 * (numRows - 1) # to get the next character in same row
-
-             for i in range(r, len(s), increment):
+            for i in range(r, len(s), increment):
                 result += s[i]
 
-                # for middle rows, we have an extra character to be added - its like a V
-                # i + increment - 2 * r = formula for it, it decreases as we go deep in V
-                if (r > 0 and r < numRows - 1 and 
-                    i + increment - 2 * r < len(s)):
-                    result += s[i + increment - 2 * r]
+                if r > 0 and r < numRows -1 and (i + increment - 2 * r) < len(s):
+                    result += s[i + increment - 2 * r] # in middle rows, number of characters increase - part of same section
         
         return result
+            
