e23b070d51c8062346ab678f61a2dbe688e19a72
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-28 20:14:58 -0500

    Time: 239 ms (30.68%), Space: 19.7 MB (17.71%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -5,11 +5,11 @@ class Solution:
         if grid[0][0] == 1 or grid[rows-1][cols-1] == 1:
             return -1
         
-        heap = [(1, 0, 0)]
+        queue = deque([(1, 0, 0)])
         visited = set([(0, 0)])
 
-        while heap:
-            dst, r, c = heapq.heappop(heap)
+        while queue:
+            dst, r, c = queue.popleft()
 
             if r == rows - 1 and c == cols - 1:
                 return dst
@@ -18,6 +18,6 @@ class Solution:
                 nr, nc = r + dr, c + dc
                 if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 0 and (nr, nc) not in visited:
                     visited.add((nr, nc))
-                    heapq.heappush(heap, (dst + 1, nr, nc))
+                    queue.append((dst + 1, nr, nc))
         
         return -1
