0c6c3047d4876a8076156355c0594ae9392d242f
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-29 03:23:48 -0500

    Time: 3 ms (16.02%), Space: 17.8 MB (70.25%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -1,24 +1,22 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        n = len(costs)//2
-        diff = []
+        delta = []
         total = 0
 
         for i, cst in enumerate(costs):
             acost, bcost = cst
-            diff.append([acost - bcost, i])
+            delta.append([acost - bcost, i])
         
-        diff.sort(key= lambda x: x[0])
-        print(diff)
-        i, j = 0, len(diff) - 1
+        delta.sort()
+        low, high = 0, len(delta) -1
 
-        while i < n:
-            indA, indB = diff[i][1], diff[j][1]
-            acost, bcost = costs[indA][0], costs[indB][1]
+        while low < high:
+            l, h = delta[low][1], delta[high][1]
+            acost, bcost = costs[l][0], costs[h][1]
             total += acost + bcost
 
-            i += 1
-            j -= 1
+            low += 1
+            high -= 1
         
         return total
         
