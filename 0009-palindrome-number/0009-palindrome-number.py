e2c6b3e18e0bc07b195dc7ff547631f754fc2bb5
Ayushi Sharma <77693531+ayushis380@users.noreply.github.com>
2025-11-29 16:49:02 -0500

    Time: 7 ms (62.66%), Space: 17.7 MB (88.55%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,6 +1,13 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        str_x = str(x)
-        print(str_x)
+        if x < 0 or (x % 10 == 0 and x != 0): # negative and which have 0 at end - wont be palind(except 0)
+            return False
+        
+        rev = 0
 
-        return str_x == str_x[::-1]
+        while x > rev: # only take the first half to compare with second half
+            rev = rev * 10 + x % 10
+            x = x//10 # O(log (base10(n)) - as /10 at each step
+        
+        # print("x is ", x, " rev is ", rev)
+        return x == rev or x == rev//10 # 12321 - here rev will be 123, odd length can drop the middle element
